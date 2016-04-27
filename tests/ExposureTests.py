6e73ddc2d0bbcae1428a812d8feaa9f70cfbb8e7
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-04-27 22:39:01 +0100

    Added delete tests

--- tests/ExposureTests.py
+++ tests/ExposureTests.py
@@ -79,16 +79,51 @@ class Test_ExposureTests(unittest.TestCase):
         assert len(exposures) == 1
 
     def test_delete_exposure_1(self):
-        os.makedirs(app.EXPOSURE_DATA_DIRECTORY)
-        return True
+        self.clean_directories()
+        filepath1 = os.path.join(app.EXPOSURE_DATA_DIRECTORY, 'test1.tar')
+        filepath2 = os.path.join(app.EXPOSURE_DATA_DIRECTORY, 'test2.tar')
+        self.touch(filepath1)
+        self.touch(filepath2)
+        response = self.app.delete("/exposure")
+        assert response._status_code == 200
+        assert not os.path.exists(filepath1)
+        assert not os.path.exists(filepath2)
 
-    def test_delete_exposure_by_id_1(self):
-        os.makedirs(app.EXPOSURE_DATA_DIRECTORY)
-        return True
+    def test_delete_exposure_by_location_1(self):
+        self.clean_directories()
+        filepath1 = os.path.join(app.EXPOSURE_DATA_DIRECTORY, 'test1.tar')
+        filepath2 = os.path.join(app.EXPOSURE_DATA_DIRECTORY, 'test2.tar')
+        self.touch(filepath1)
+        self.touch(filepath2)
+        response = self.app.delete("/exposure/test1")
+        assert response._status_code == 200
+        assert not os.path.exists(filepath1)
+        assert os.path.exists(filepath2)
 
     def test_exposure_roundtrip_1(self):
-        os.makedirs(app.EXPOSURE_DATA_DIRECTORY)
-        return True
+        self.clean_directories()
+        filepath = os.path.join(TEST_DIRECTORY, 'post1.tar')
+        self.touch(filepath)
+        with io.open(filepath, 'rb') as file_to_upload:
+            response = self.app.post(
+                "/exposure", 
+                data = {
+                    'file': (file_to_upload, filepath),
+                }, 
+                content_type='multipart/form-data')
+        assert response._status_code == 200
+        exposures = json.loads(response.data.decode('utf-8'))['exposures']
+        assert len(exposures) == 1
+        location = exposures[0]['location']
+        response = self.app.get("/exposure/" + location)
+        assert response._status_code == 200
+        exposures = json.loads(response.data.decode('utf-8'))['exposures']
+        assert len(exposures) == 1
+        assert sum(1 for exposure in exposures if exposure['location'] == location) == 1
+        response = self.app.delete("/exposure/" + location)
+        assert response._status_code == 200
+        response = self.app.get("/exposure/" + location)
+        assert response._status_code == 404
 
 if __name__ == '__main__':
     unittest.main()
