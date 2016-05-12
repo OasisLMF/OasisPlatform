501bbf29ccc7048fbd5ed609fb42256cc9851b0b
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-05-12 12:47:09 +0100

    Updated structure of project

--- tests/AnalysisTester.py
+++ tests/AnalysisTester.py
@@ -5,18 +5,21 @@ import sys
 import unittest
 from flask import json
 import shutil
+
 import tarfile
 import time
 
 TEST_DIRECTORY = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 TEST_DATA_DIRECTORY = os.path.abspath(os.path.join(TEST_DIRECTORY, 'data'))
-LIB_PATH = os.path.abspath(os.path.join(TEST_DIRECTORY, '..', 'src'))
-sys.path.append(LIB_PATH)
+sys.path.append(os.path.join(TEST_DIRECTORY, '..', 'src', 'server'))
+sys.path.append(os.path.join(TEST_DIRECTORY, '..', 'src', 'common'))
 
 import app
 import TaskStatus
+import common.resource_classes
 
 app.DO_GZIP_RESPONSE = False
+sys.path.append(os.path.join(TEST_DIRECTORY, '..', 'src'))
 exposure_data_directory = os.path.join(TEST_DIRECTORY, 'exposure_data')
 app.EXPOSURE_DATA_DIRECTORY = exposure_data_directory
 app.APP.config['TESTING'] = True
@@ -28,8 +31,8 @@ location = json.loads(response.data.decode('utf-8'))['location']
 status = TaskStatus.TASK_STATUS_PENDING
 
 while True:
-    url ="/analysis_queue/" + str(location) 
-    response = app.get("/analysis_queue/" + str(location))
+    url ="/analysis_status/" + str(location) 
+    response = app.get("/analysis_status/" + str(location))
     status = str(json.loads(response.data.decode('utf-8'))['status'])
     if status == TaskStatus.TASK_STATUS_SUCCESS:
         break
