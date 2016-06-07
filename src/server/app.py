17e662b85a3ea41069f2c508275f8d37f022db96
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-06-07 11:01:10 +0100

    Various updates

--- src/server/app.py
+++ src/server/app.py
@@ -9,12 +9,15 @@ import logging
 import tarfile
 import time
 import traceback
-from ConfigParser import ConfigParser
-from flask import Flask, Response, request, jsonify
-from flask_swagger import swagger
+
 from celery import Celery
 from common import data
 from common import helpers
+from ConfigParser import ConfigParser
+from flask import Flask, Response, request, jsonify
+from flask_swagger import swagger
+from flask.helpers import send_from_directory
+from logging.handlers import RotatingFileHandler
 
 APP = Flask(__name__)
 
@@ -22,29 +25,37 @@ CONFIG_PARSER = ConfigParser()
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 INI_PATH = os.path.abspath(os.path.join(CURRENT_DIRECTORY, 'OasisApi.ini'))
 CONFIG_PARSER.read(INI_PATH)
+LOG_FILE = CONFIG_PARSER.get('Default', 'LOG_FILE')
+LOG_LEVEL = CONFIG_PARSER.get('Default', 'LOG_LEVEL')
+LOG_NUMERIC_LEVEL = getattr(logging, LOG_LEVEL.upper(), logging.DEBUG)
+LOG_MAX_SIZE_IN_BYTES = CONFIG_PARSER.get('Default', 'LOG_MAX_SIZE_IN_BYTES')
+LOG_BACKUP_COUNT = CONFIG_PARSER.get('Default', 'LOG_BACKUP_COUNT')
 
-EXPOSURE_DATA_DIRECTORY = CONFIG_PARSER.get('Default', 'EXPOSURE_DATA_DIRECTORY')
-RESULTS_DATA_DIRECTORY = CONFIG_PARSER.get('Default', 'RESULTS_DATA_DIRECTORY')
+HANDLER = RotatingFileHandler(
+    LOG_FILE, maxBytes=LOG_MAX_SIZE_IN_BYTES, backupCount=LOG_BACKUP_COUNT)
+APP.logger.setLevel(logging.DEBUG)
+APP.logger.addHandler(HANDLER)
+FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+HANDLER.setFormatter(FORMATTER)
 
-DATA_FILE_SUFFIX = '.tar'
+INPUTS_DATA_DIRECTORY = CONFIG_PARSER.get('Default', 'INPUTS_DATA_DIRECTORY')
+OUTPUTS_DATA_DIRECTORY = CONFIG_PARSER.get('Default', 'OUTPUTS_DATA_DIRECTORY')
+
+TAR_FILE_SUFFIX = '.tar'
 GZIP_FILE_SUFFIX = '.gz'
-
-HTTP_RESPONSE_OK = 200
-HTTP_RESPONSE_INTERNAL_ERROR = 400
-HTTP_RESPONSE_RESOURCE_NOT_FOUND = 404
 
 CELERY = Celery()
 CELERY.config_from_object('common.CeleryConfig')
 
 @APP.route('/exposure_summary', defaults={'location': None}, methods=["GET"])
 @APP.route('/exposure_summary/<location>', methods=["GET"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def get_exposure_summary(location):
     """
     Get exposure summary
     ---
     definitions:
-    - schema:
+     schema:
         id: ExposureSummary
         properties:
             location:
@@ -68,6 +79,7 @@ def get_exposure_summary(location):
                 items:
                     $ref: "#/definitions/ExposureSummary"
         404:
+
             description: Resource not found
     parameters:
     - name: location
@@ -77,13 +89,14 @@ def get_exposure_summary(location):
       type: string
     """
     try:
+        APP.logger.debug("Location: {}".format(location))
         if location is None:
             exposure_summaries = list()
-            for filename in os.listdir(EXPOSURE_DATA_DIRECTORY):
+            for filename in os.listdir(INPUTS_DATA_DIRECTORY):
 
-                filepath = os.path.join(EXPOSURE_DATA_DIRECTORY, filename)
+                filepath = os.path.join(INPUTS_DATA_DIRECTORY, filename)
                 
-                if not filepath.endswith(DATA_FILE_SUFFIX):
+                if not filepath.endswith(TAR_FILE_SUFFIX):
                     continue
                 if not os.path.isfile(filepath):
                     continue
@@ -92,35 +105,35 @@ def get_exposure_summary(location):
                 created_date = time.ctime(os.path.getctime(filepath))
                 exposure_summaries.append(
                     ExposureSummary(
-                        location=str.replace(filename, DATA_FILE_SUFFIX, ''),
+                        location=str.replace(filename, TAR_FILE_SUFFIX, ''),
                         size=size_in_bytes,
                         created_date=created_date))
             response = jsonify(
                 {"exposures": [exposure_summary.__dict__ for exposure_summary in exposure_summaries]})
         else:
-            filename = str(location) + DATA_FILE_SUFFIX
-            filepath = os.path.join(EXPOSURE_DATA_DIRECTORY, filename)
+            filename = str(location) + TAR_FILE_SUFFIX
+            filepath = os.path.join(INPUTS_DATA_DIRECTORY, filename)
 
             if not os.path.exists(filepath):
-                response = Response(status=HTTP_RESPONSE_RESOURCE_NOT_FOUND)
+                response = Response(status=helpers.HTTP_RESPONSE_RESOURCE_NOT_FOUND)
             else:
                 size_in_bytes = os.path.getsize(filepath)
                 created_date = time.ctime(os.path.getctime(filepath))
                 
                 exposure_summary = ExposureSummary(
-                    location=str.replace(filename, DATA_FILE_SUFFIX, ''),
+                    location=str.replace(filename, TAR_FILE_SUFFIX, ''),
                     size=size_in_bytes,
                     created_date=created_date)
                 response = jsonify({"exposures": [exposure_summary.__dict__]})
+                APP.logger.debug("Exposures: " + response.data)
     except:
-        print "Error in post_lookup"
-        print traceback.format_exc()
-        response = Response(status=HTTP_RESPONSE_INTERNAL_ERROR)
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
 
     return response
 
 @APP.route('/exposure/<location>', methods=["GET"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def get_exposure(location):
     """
     Get an exposure resource
@@ -141,10 +154,17 @@ def get_exposure(location):
       required: true
       type: string
     """
-    return True
+    # TODO
+    try:
+        True
+    except:
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+
+    return response
 
 @APP.route('/exposure', methods=["POST"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def post_exposure():
     """
     Upload an exposure resource
@@ -170,8 +190,8 @@ def post_exposure():
             is_gzipped = (content_encoding == 'gzip')
 
         file = request.files['file']
-        filename = str(uuid.uuid4())
-        filepath = os.path.join(EXPOSURE_DATA_DIRECTORY, filename) + DATA_FILE_SUFFIX
+        filename = helpers.generate_unique_filename()
+        filepath = os.path.join(INPUTS_DATA_DIRECTORY, filename) + TAR_FILE_SUFFIX
         file.save(filepath)
 
         # If zipped, extract the tar file
@@ -189,18 +209,15 @@ def post_exposure():
         }  
 
         response = jsonify({'exposures': [exposure]})
-
     except:
-        print "Error in post_lookup"
-        print traceback.format_exc()
-        response = Response(status=HTTP_RESPONSE_INTERNAL_ERROR)
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
 
     return response
 
 @APP.route('/exposure', defaults={'location': None}, methods=["DELETE"])
 @APP.route('/exposure/<location>', methods=["DELETE"])
-@helpers.oasis_log
-
+@helpers.oasis_log(APP.logger)
 def delete_exposure(location):
     """
     Delete an exposure resource
@@ -221,39 +238,38 @@ def delete_exposure(location):
       type: string
     """
     try:
+        APP.logger.debug("Location: {}".format(location))
         if location is None:
 
-            for filename in os.listdir(EXPOSURE_DATA_DIRECTORY):
+            for filename in os.listdir(INPUTS_DATA_DIRECTORY):
                 
-                filepath = os.path.join(EXPOSURE_DATA_DIRECTORY, filename)
+                filepath = os.path.join(INPUTS_DATA_DIRECTORY, filename)
     
-                if not filepath.endswith(DATA_FILE_SUFFIX):
+                if not filepath.endswith(TAR_FILE_SUFFIX):
                     continue
                 if not os.path.isfile(filepath):
                     continue
                 os.remove(filepath)
-            response = Response(status=HTTP_RESPONSE_OK)
+            response = Response(status=helpers.HTTP_RESPONSE_OK)
         
         else:
             
-            filename = str(location) + DATA_FILE_SUFFIX;
-            filepath = os.path.join(EXPOSURE_DATA_DIRECTORY, filename)
+            filename = str(location) + TAR_FILE_SUFFIX;
+            filepath = os.path.join(INPUTS_DATA_DIRECTORY, filename)
 
             if not os.path.exists(filepath):
-                response = Response(status=HTTP_RESPONSE_RESOURCE_NOT_FOUND)
+                response = Response(status=helpers.HTTP_RESPONSE_RESOURCE_NOT_FOUND)
             else:
                 os.remove(filepath)
-                response = Response(status=HTTP_RESPONSE_OK)
-        
+                response = Response(status=helpers.HTTP_RESPONSE_OK)
     except:
-        print "Error in post_lookup"
-        print traceback.format_exc()
-        response = Response(status=HTTP_RESPONSE_INTERNAL_ERROR)
+        APP.log_exception(traceback.format_exc())
+        response = Response(status_code.HTTP_RESPONSE_INTERNAL_ERROR) 
 
     return response
 
 @APP.route('/analysis', methods=["POST"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def post_analysis():
     """
     Start an analysis
@@ -275,12 +291,21 @@ def post_analysis():
       required: true
       type: file
     """
-    result = CELERY.send_task("tasks.start_analysis", ["ANALYSIS_SETTINGS_JSON"])
-    task_id = result.task_id
-    return jsonify({'location': task_id})
-    
+    try:
+        analysis_settings = request.json
+        if not validate_analysis_settings(analysis_settings):
+            response = Response(status_code = HTTP_RESPONSE_BAD_REQUEST)
+        else:
+            result = CELERY.send_task("tasks.start_analysis", [analysis_settings])
+            task_id = result.task_id
+            response = jsonify({'location': task_id})
+    except:
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+    return response
+
 @APP.route('/analysis_status/<location>', methods=["GET"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def get_analysis_status(location):
     """
     Get an analysis status resource
@@ -298,9 +323,23 @@ def get_analysis_status(location):
             message:
                 type: string
                 description: The analysis status message.
-            outputs_summary_location:
+            outputs_summary:
+                type: OutputsSummary
+                description: Summary of the outputs.
+        id: OutputsSummary
+
+        properties:
+            location:
                 type: string
-                description: The location of the analysis outputs.
+                description: The location of the data.
+            size:
+                type: integer
+                description: The size of the uncompressed data in bytes.
+            created_date:
+                type: string
+                format: dateTime
+
+                description: The date when the data was created.
     description: Gets an analysis status resource. If no location is given all exposure status resources are returned. 
     produces:
     - application/json
@@ -320,13 +359,39 @@ def get_analysis_status(location):
         required: true
         type: string
     """
-    result = CELERY.AsyncResult(location)
-    status = result.status
-    return jsonify({'status': status})
+    try:
+        result = CELERY.AsyncResult(location)
+        APP.logger.debug("celery result:{}".format(result.result))
+       
+        if result.state == helpers.TASK_STATUS_SUCCESS: 
+            analysis_status = data.AnalysisStatus(
+                id = -1,
+                status = result.state,
+                message="",             
+                outputs_location=result.result)
+        elif result.state == helpers.TASK_STATUS_FAILURE: 
+            analysis_status = data.AnalysisStatus(
+                id = -1,
+                status = result.state,
+                message=result.result,             
+                outputs_location=None)
+        else: 
+            analysis_status = data.AnalysisStatus(
+                id = -1,
+                status = result.state,
+                message="",             
+                outputs_location=None)
+         
+        response = jsonify(analysis_status.__dict__)
+        APP.logger.debug("Response: {}".format(response.data))
+    except:
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+    return response
 
 @APP.route('/analysis_status', methods=["DELETE"])
 @APP.route('/analysis_status/<location>', methods=["DELETE"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def delete_analysis_status(location):
     """
     Delete an analysis status resource
@@ -346,11 +411,16 @@ def delete_analysis_status(location):
       required: true
       type: string
     """
-    #TODO
-    return True
+    try:
+        #TODO
+        APP.logger.debug("Location: {}".format(location))
+    except:
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+    return response
 
 @APP.route('/outputs/<location>', methods=["GET"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def get_outputs(location):
     """
     Get a outputs resource
@@ -370,13 +440,24 @@ def get_outputs(location):
         required: true
         type: string
     """
-    return True
+    try:
+        APP.logger.debug("Location: {}".format(location))
+        file_path = os.path.join(OUTPUTS_DATA_DIRECTORY, location + ".tar") 
+        if not os.path.exists(file_path):
+            response = Response(status_code = helpers.HTTP_RESPONSE_RESOURCE_NOT_FOUND)
+        else:
+            response = send_from_directory(OUTPUTS_DATA_DIRECTORY, location + ".tar")
+    except:
+        APP.log_exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+    return response
 
 @APP.route('/outputs', methods=["DELETE"])
 @APP.route('/outputs/<location>', methods=["DELETE"])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def delete_outputs(location):
     """
+    download_directory = '/var/www/oasis/download'
     Delete a outputs resource
     ---
     description: Deletes a outputs resource. If no location is given all outputs resources will be deleted.
@@ -394,10 +475,40 @@ def delete_outputs(location):
       required: true
       type: string
     """
-    return True
+    try:
+        APP.logger.debug("Location: {}".format(location))
+        if location is None:
+
+            for filename in os.listdir(OUTPUTS_DATA_DIRECTORY):
+                
+                filepath = os.path.join(OUTPUTS_DATA_DIRECTORY, filename)
+    
+                if not filepath.endswith(TAR_FILE_SUFFIX):
+                    continue
+                if not os.path.isfile(filepath):
+                    continue
+                os.remove(filepath)
+            response = Response(status=helpers.HTTP_RESPONSE_OK)
+        
+        else:
+            
+            filename = str(location) + TAR_FILE_SUFFIX;
+            filepath = os.path.join(OUTPUTS_DATA_DIRECTORY, filename)
+
+            if not os.path.exists(filepath):
+                response = Response(status=helpers.HTTP_RESPONSE_RESOURCE_NOT_FOUND)
+            else:
+                os.remove(filepath)
+                response = Response(status=helpers.HTTP_RESPONSE_OK)
+        
+    except:
+        APP.logger.exception(traceback.format_exc())
+        response = Response(status=helpers.HTTP_RESPONSE_INTERNAL_ERROR)
+
+    return response
 
 @APP.route("/spec")
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def spec():
     swag = swagger(APP)
     swag['info']['version'] = "0.1"
@@ -405,7 +516,7 @@ def spec():
     return jsonify(swag)
 
 @APP.route('/healthcheck', methods=['GET'])
-@helpers.oasis_log
+@helpers.oasis_log(APP.logger)
 def get_healthcheck():
     '''
     Basic healthcheck response.
@@ -427,3 +538,7 @@ def validate_exposure_tar(filepath):
         (sum(1 for member in members if member.name == 'fm_policytc.bin') == 1) and \
         (sum(1 for member in members if member.name == 'fm_profile.bin') == 1) and \
         (sum(1 for member in members if member.name == 'fm_summaryxref.bin') == 1)
+
+def validate_analysis_settings(analysis_settings_json):
+    # TODO
+    return True
