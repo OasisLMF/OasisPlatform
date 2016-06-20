863b069dd285c259eef612558b68ec6fd9008f33
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-06-20 16:12:28 +0100

    Further work

--- src/model_execution_worker/OasisIM/supplier_model_runner.py
+++ src/model_execution_worker/OasisIM/supplier_model_runner.py
@@ -5,7 +5,7 @@ import shutil
 import tempfile
 import logging
 import subprocess
-from model_execution import model_executor
+from model_execution import model_runner
 from common import helpers
 from billiard import Process, Queue, cpu_count
 
@@ -14,4 +14,4 @@ def run(analysis_settings, number_of_processes=-1):
     if number_of_processes == -1:
         number_of_processes = cpu_count()
 
-    model_executor.run_analysis(analysis_settings, number_of_processes)
+    model_runner.run_analysis(analysis_settings, number_of_processes)
