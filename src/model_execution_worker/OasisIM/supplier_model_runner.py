c5151d7349fd3d6f782ed2d9241b82c6028c7602
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-06-20 15:59:07 +0100

    Added

---
+++ src/model_execution_worker/OasisIM/supplier_model_runner.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import json
+import shutil
+import tempfile
+import logging
+import subprocess
+from model_execution import model_executor
+from common import helpers
+from billiard import Process, Queue, cpu_count
+
+def run(analysis_settings, number_of_processes=-1):
+ 
+    if number_of_processes == -1:
+        number_of_processes = cpu_count()
+
+    model_executor.run_analysis(analysis_settings, number_of_processes)
