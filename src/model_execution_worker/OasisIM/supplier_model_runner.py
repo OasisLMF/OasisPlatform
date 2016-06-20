8a024bf5e8403ec736f1584a2cfcba420b102093
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-06-20 23:59:22 +0100

    Further work

--- src/model_execution_worker/OasisIM/supplier_model_runner.py
+++ src/model_execution_worker/OasisIM/supplier_model_runner.py
@@ -1,17 +1,11 @@
-import os
-import sys
-import json
-import shutil
-import tempfile
 import logging
-import subprocess
+import multiprocessing
 from model_execution import model_runner
-from common import helpers
-from billiard import Process, Queue, cpu_count
+
 
 def run(analysis_settings, number_of_processes=-1):
- 
+
     if number_of_processes == -1:
-        number_of_processes = cpu_count()
+        number_of_processes = multiprocessing.cpu_count()
 
     model_runner.run_analysis(analysis_settings, number_of_processes)
