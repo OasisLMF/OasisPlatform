b550383250b1d0f3a710fc72568853ed020e2331
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-06-20 22:21:16 +0100

    Removed

--- src/calcbe_worker/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod a+w /var/www/oasis/upload
-chmod a+w /var/www/oasis/download
-
-# Start worker on init
-celery worker --detach --config=common.CeleryConfig --loglevel=INFO --logfile="/var/log/oasis/%n.log"
