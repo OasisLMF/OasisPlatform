291224dbe8e7bf039ef206b300b3db2145403581
RonKalian <ron@kalian.co.uk>
2016-09-07 17:05:18 +0100

    final fix for JSON tester data

--- tests/JSON2cmd_data/all.cmd
+++ tests/JSON2cmd_data/all.cmd
@@ -11,15 +11,15 @@ mkfifo working/il1summary1summarycalc
 mkfifo working/il1summary1eltcalc
 mkfifo working/il1summary1aalcalc
 mkfifo working/il1summary1pltcalc
-tee < working/gul1summary1 working/gul1summary1summarycalc working/gul1summary1eltcalc working/gulsummary1/p1.bin working/gul1summary1aalcalc  > working/gul1summary1pltcalc
-tee < working/il1summary1 working/il1summary1summarycalc working/il1summary1eltcalc working/ilsummary1/p1.bin working/il1summary1aalcalc  > working/il1summary1pltcalc
+tee < working/gul1summary1 working/gul1summary1summarycalc working/gul1summary1eltcalc work/gulsummary1/p1.bin working/gul1summary1aalcalc  > working/gul1summary1pltcalc
+tee < working/il1summary1 working/il1summary1summarycalc working/il1summary1eltcalc work/ilsummary1/p1.bin working/il1summary1aalcalc  > working/il1summary1pltcalc
 summarycalctocsv < working/gul1summary1summarycalc > working/gul_1_summarycalc_1
 eltcalc < working/gul1summary1eltcalc > working/gul_1_eltcalc_1
-aalcalc < working/gul1summary1aalcalc > working/gulaalSummary1/p1.bin
+aalcalc < working/gul1summary1aalcalc > work/gulaalSummary1/p1.bin
 pltcalc < working/gul1summary1pltcalc > working/gul_1_pltcalc_1
 summarycalctocsv < working/il1summary1summarycalc > working/il_1_summarycalc_1
 eltcalc < working/il1summary1eltcalc > working/il_1_eltcalc_1
-aalcalc < working/il1summary1aalcalc > working/ilaalSummary1/p1.bin
+aalcalc < working/il1summary1aalcalc > work/ilaalSummary1/p1.bin
 pltcalc < working/il1summary1pltcalc > working/il_1_pltcalc_1
 summarycalc -g  -1 working/gul1summary1 < working/gul1
 summarycalc -f  -1 working/il1summary1 < working/il1
