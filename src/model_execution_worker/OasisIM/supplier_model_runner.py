b3f3f74bf14a12d38aefbd82ecec1a8b41dc57cb
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-09-26 11:19:37 +0100

    Temp cap concurrent jobs to 5, while investigating issues in job concurrency

--- src/model_execution_worker/OasisIM/supplier_model_runner.py
+++ src/model_execution_worker/OasisIM/supplier_model_runner.py
@@ -8,4 +8,6 @@ def run(analysis_settings, number_of_processes=-1):
     if number_of_processes == -1:
         number_of_processes = multiprocessing.cpu_count()
 
+    number_of_processes = 5
+
     model_runner.run_analysis(analysis_settings, number_of_processes)
