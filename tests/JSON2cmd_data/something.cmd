81265fbe59e1321894da224fa6d7a87bed608c2e
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-10-17 14:21:10 +0100

    Removed old json to command tests

--- tests/JSON2cmd_data/something.cmd
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkfifo working/gul1
-mkfifo working/gul1summary1
-mkfifo working/gul1summary1eltcalc
-tee < working/gul1summary1  > working/gul1summary1eltcalc
-eltcalc < working/gul1summary1eltcalc > working/gul_1_eltcalc_1
-summarycalc -g  -1 working/gul1summary1 < working/gul1
-eve 1 1 | getmodel | gulcalc -S100  -L100 -c working/gul1 
-kat working/gul_1_eltcalc_1 > output/gul_1_eltcalc.csv
