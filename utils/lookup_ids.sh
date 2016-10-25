e7081b3e0a3e4e4598e820566a0f6fbea69c01b2
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2016-10-25 16:49:23 +0100

    Fixed ordering before join

--- utils/lookup_ids.sh
+++ utils/lookup_ids.sh
@@ -6,5 +6,5 @@
 # Does a reverse looup to produce an ELT with the source location IDs
 #
 
-echo 'event_id,type,location_desc,mean'
-join -t , -1 1 -2 1 <(csvgrep -c mean -i -r "^0" $1 | grep -v Summary_id elt.csv | cut -d , -f 1,2,3,4 | sort -t , -k 1,1n) <(grep -v item_id $2 | cut -d , -f 4,5 | uniq | sort -t , -k 1,1n) | sort -t , -k 3n,3 -k 5n,5 | sed -e 's/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\)$/\3,\2,\5,\4/'
+echo 'event_id,type,location_desc,mean,std_dev,exposure_value'
+join -t , -1 1 -2 1 <(csvgrep -c mean -i -r "^0" $1 | grep -v Summary_id | cut -d , -f 1,2,3,4,5,6 | sort -t , -k 1,1) <(grep -v item_id $2 | cut -d , -f 4,5 | uniq | sort -t , -k 1,1) | sort -t , -k 3n,3 -k 5n,5 | sed -e 's/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\)$/\3,\2,\7,\4,\5,\6/'
