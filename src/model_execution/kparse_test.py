a93e4be5552b3249d71e53f61d93b911b8ee2b0b
Ben <neb@sky.com>
2017-03-27 14:41:16 +0100

    fixed kparse_test

--- src/model_execution/kparse_test.py
+++ src/model_execution/kparse_test.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python
-
-from  kparse import *
+"""
+        Test harness for kparse.py
+"""
 import getopt
+import sys
+import json
+from pprint import pprint
+import model_execution.kparse as kparse
+
+#pylint: disable=I0011,C0111
 
 def usage():
-	print "-a analysis settings"
-	print "-p No of Processors"
+    print "-a analysis settings"
+    print "-p No of Processors"
 
 def main(argv):
-	processor_count=1
-	analysis_filename=""
-	try:
-		opts, args = getopt.getopt(argv, "p:a:", ["help", "grammar="])
+    processor_count = 1
+    analysis_filename = ""
+    output_file = "output.sh"
+    try:
+        opts, _ = getopt.getopt(argv, "p:a:o:", ["help", "grammar="])
 
-	except getopt.GetoptError:
-		usage()
-		sys.exit()
-	# 
-	for opt, arg in opts:
-		if opt in ("-p"):
-			processor_count=int(arg)						
-		elif opt in ("-a"):
-			analysis_filename = arg
+    except getopt.GetoptError:
+        usage()
+        sys.exit()
 
-	genbash(processor_count,analysis_filename)
+    for opt, arg in opts:
+        if opt in "-p":
+            processor_count = int(arg)
+        elif opt in "-a":
+            analysis_filename = arg
+        elif opt in "-o":
+            output_file = arg
+
+    with open(analysis_filename) as data_file:
+        analysis_data = json.load(data_file)
+
+    # pprint(analysis_data)
+    kparse.genbash(processor_count, analysis_data["analysis_settings"], output_file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
