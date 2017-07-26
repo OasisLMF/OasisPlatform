052ba83477944ca531d4d9435d8353697ce8cbe1
Sandeep Murthy <sr-murthy@users.noreply.github.com>
2017-07-26 10:07:24 +0100

    Update `api_tester.py` - make `print` a function

--- src/utils/api_tester.py
+++ src/utils/api_tester.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 # Python 2 standard library imports
 import argparse
 import inspect
@@ -48,16 +50,14 @@ input_data_directory = args.input_data_directory
 output_data_directory = args.output_data_directory
 do_verbose = args.verbose
 
-api_url = 'http://{0}'.format(api_ip)
+api_url = 'http://{}'.format(api_ip)
 
 if not os.path.exists(input_data_directory):
-    print "Input data directory does not exist: {}".format(
-        analysis_settings_json_filepath)
+    print("Input data directory does not exist: {}".format(analysis_settings_json_filepath))
     exit()
 
 if not os.path.exists(analysis_settings_json_filepath):
-    print "Analysis settings file does not exist: {}".format(
-        analysis_settings_json_filepath)
+    print("Analysis settings file does not exist: {}".format(analysis_settings_json_filepath))
     exit()
 
 if not os.path.exists(output_data_directory):
@@ -128,5 +128,4 @@ for t in analysis_threads:
 for t in analysis_threads:
     t.join()
 
-print "Done. Num completed={}; Num failed={}".format(
-    c.num_completed, c.num_failed)
+print("Done. Num completed={}; Num failed={}".format(c.num_completed, c.num_failed))
