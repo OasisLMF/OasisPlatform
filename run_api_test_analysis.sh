d0b2d43ad4d85eeba102658bf1df46646f9d8a3d
Paul Dragoonis <dragoonis@gmail.com>
2017-12-04 13:37:52 +0000

    Integration test mods

--- run_api_test_analysis.sh
+++ run_api_test_analysis.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python src/oasisapi_client/model_api_tester.py -s http://oasis_api_server -a tests/data/analysis_settings.json -i tests/data/input -o tests/data/output
+pip install -r /var/www/oasis/oasisapi_client/requirements.txt
+pip install -r /var/www/oasis/model_execution_worker/requirements.txt
+
+python oasisapi_client/model_api_tester.py -s http://oasis_api_server -a tests/data/analysis_settings.json -i tests/data/input -o tests/data/output
