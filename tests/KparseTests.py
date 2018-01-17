b3553a5c19009d78cd181beae6869a6fc3da32bf
Paul Dragoonis <dragoonis@gmail.com>
2018-01-17 15:49:25 +0000

    KParseTests.py

--- tests/KparseTests.py
+++ tests/KparseTests.py
@@ -17,7 +17,7 @@ TEST_DATA_DIRECTORY = os.path.abspath(os.path.join(TEST_DIRECTORY, 'data'))
 LIB_PATH = os.path.abspath(os.path.join(TEST_DIRECTORY, os.pardir, 'src'))
 sys.path.append(LIB_PATH)
 
-from model_execution import kparse
+import kparse
 
 KPARSE_INPUT_FOLDER = os.path.join(TEST_DIRECTORY, "kparse_input")
 KPARSE_OUTPUT_FOLDER = os.path.join(TEST_DIRECTORY, "kparse_output")
