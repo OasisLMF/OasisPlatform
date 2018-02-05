fa37386d332494e6174cfc4d4203e8f0ba41852f
Paul Dragoonis <dragoonis@gmail.com>
2018-02-05 15:26:36 +0000

    catrisks integration model tests

--- src/server/startup.sh
+++ src/server/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod a+w /var/www/oasis/upload
 chmod a+w /var/www/oasis/download
-chown -R worker:worker /var/www/oasis/upload
-chown -R worker:worker /var/www/oasis/download
+chown -R oasis:oasis /var/www/oasis/upload
+chown -R oasis:oasis /var/www/oasis/download
 
 apachectl -k start -DFOREGROUND
