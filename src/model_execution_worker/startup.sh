71c704abc5e7965106b3340285c14ec7452ceeb9
Paul Dragoonis <dragoonis@gmail.com>
2018-02-06 12:00:52 +0000

    CI work

--- src/model_execution_worker/startup.sh
+++ src/model_execution_worker/startup.sh
@@ -6,12 +6,11 @@ rm -f /home/worker/celeryd.pid
 
 sed -i -e "s/%RABBIT_PORT%/$RABBIT_PORT/" /home/worker/common/CeleryConfig.py
 sed -i -e "s/%MYSQL_PORT%/$MYSQL_PORT/" /home/worker/common/CeleryConfig.py
-sed -i -e "s/%CELERY_QUEUE%/$MODEL_SUPPLIER_ID-$MODEL_VERSION_ID/" startup.sh
 
 ./utils/wait-for-it.sh "oasis_rabbit:$RABBIT_PORT"
 ./utils/wait-for-it.sh "oasis_mysql:$MYSQL_PORT"
 
 # Start worker on init
-celery worker -A model_execution_worker.tasks --detach --loglevel=INFO --logfile="/var/log/oasis/worker.log" -Q %CELERY_QUEUE%
+celery worker -A model_execution_worker.tasks --detach --loglevel=INFO --logfile="/var/log/oasis/worker.log" -Q ${MODEL_SUPPLIER_ID}/${MODEL_VERSION_ID}
 
 tail -f /var/log/oasis/worker.log
