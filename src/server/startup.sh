53819eec3d17c17c79c4815141131f1a2ee9e3e5
Paul Dragoonis <dragoonis@gmail.com>
2018-02-06 09:50:19 +0000

    CI work

--- src/server/startup.sh
+++ src/server/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 chmod a+w /var/www/oasis/upload
 chmod a+w /var/www/oasis/download
-chown -R oasis:oasis /var/www/oasis/upload
-chown -R oasis:oasis /var/www/oasis/download
+chown -R oasis:www-data /var/www/oasis/upload
+chown -R oasis:www-data /var/www/oasis/download
 
-apachectl -k start -DFOREGROUND
+# apachectl -k start -DFOREGROUND
+apachectl
