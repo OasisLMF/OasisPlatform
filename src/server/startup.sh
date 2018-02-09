b425f21e2ce82fe29ae5213a46d3026cc3fc0389
Paul Dragoonis <dragoonis@gmail.com>
2018-02-09 18:03:42 +0000

    CI Testing

--- src/server/startup.sh
+++ src/server/startup.sh
@@ -3,8 +3,8 @@ set -e
 
 chmod a+w /var/www/oasis/upload
 chmod a+w /var/www/oasis/download
-chown -R oasis:www-data /var/www/oasis/upload
-chown -R oasis:www-data /var/www/oasis/download
+chown -R www-data:www-data /var/www/oasis/upload
+chown -R www-data:www-data /var/www/oasis/download
 
 # apachectl -k start -DFOREGROUND
 apachectl start
