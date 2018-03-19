8c0ef50a4d0ef2322dfb49c8a605518691f2da8d
Sam Gamble <hexadessa@gmail.com>
2018-03-19 10:16:36 +0000

    Update Test Analysis script - Run OasisLMF test sub-command

--- run_api_test_analysis.sh
+++ run_api_test_analysis.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-oasislmf test model-api
-python oasisapi_client/model_api_tester.py http://oasis_api_server -a tests/data/analysis_settings.json -i tests/data/input -o tests/data/output
+oasislmf test model-api http://oasis_api_server -a tests/analysis_settings.json -i tests/input -o tests/output
