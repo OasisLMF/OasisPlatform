437bcd290d55dc44cc9bb1ea9eb001969bedde5d
Sam Gamble <hexadessa@gmail.com>
2018-05-09 10:58:59 +0100

    Fix worker image startup script

--- src/startup_worker.sh
+++ src/startup_worker.sh
@@ -10,12 +10,11 @@ export OASIS_API_INI_PATH="${SCRIPT_DIR}/conf.ini"
 # Delete celeryd.pid file - fix que pickup issues on reboot of server
 rm -f /home/worker/celeryd.pid
 
-./utils/wait-for-it.sh "oasis_rabbit:$RABBIT_PORT"
-./utils/wait-for-it.sh "oasis_mysql:$MYSQL_PORT"
+./src/utils/wait-for-it.sh "oasis_rabbit:$RABBIT_PORT"
+./src/utils/wait-for-it.sh "oasis_mysql:$MYSQL_PORT"
 
 # Start worker on init
-cd /home 
-celery worker -A worker.model_execution_worker.tasks --detach --loglevel=INFO --logfile="/var/log/oasis/worker.log" -Q "${MODEL_SUPPLIER_ID}-${MODEL_VERSION_ID}"
+celery worker -A src.model_execution_worker.tasks --detach --loglevel=INFO --logfile="/var/log/oasis/worker.log" -Q "${MODEL_SUPPLIER_ID}-${MODEL_VERSION_ID}"
 
 sleep 5
 
