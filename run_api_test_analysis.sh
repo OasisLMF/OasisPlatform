ea75317319b7240fc8cc972378ffbfa4d77ad49e
Mark Pinkerton <mark.pinkerton@oasislnf.org>
2018-08-14 19:06:03 +0100

    A few updated to run with latest Docker files and repo structure

--- run_api_test_analysis.sh
+++ run_api_test_analysis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-oasislmf test model-api http://oasis_api_server -a tests/analysis_settings.json -i tests/input/csv -o tests/output
+oasislmf test model-api http://oasis_api_server -a tests/integration/analysis_settings.json -i tests/integration/input/csv -o tests/integration/output
