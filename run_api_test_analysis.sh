b2dc31101591260ba6573cd169eca1702475b8bc
Sam Gamble <hexadessa@gmail.com>
2018-11-09 11:13:38 +0000

    Add timeout to api exec test

--- run_api_test_analysis.sh
+++ run_api_test_analysis.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-oasislmf test model-api http://oasis_api_server -a tests/integration/analysis_settings.json -i tests/integration/input/csv -o tests/integration/output
+TIMEOUT=120
+timeout $TIMEOUT oasislmf test model-api http://oasis_api_server \ 
+                             -a tests/integration/analysis_settings.json \
+                             -i tests/integration/input/csv \
+                             -o tests/integration/output
