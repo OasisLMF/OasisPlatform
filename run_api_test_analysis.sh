d397772d15b351caf888826c55fe46fe749b65b8
Sam Gamble <hexadessa@gmail.com>
2018-11-09 11:22:15 +0000

    Fix intergration test script

--- run_api_test_analysis.sh
+++ run_api_test_analysis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 TIMEOUT=120
-timeout $TIMEOUT oasislmf test model-api http://oasis_api_server \ 
-                             -a tests/integration/analysis_settings.json \
-                             -i tests/integration/input/csv \
-                             -o tests/integration/output
+timeout $TIMEOUT oasislmf test model-api http://oasis_api_server -a tests/integration/analysis_settings.json \
+                                                                 -i tests/integration/input/csv \
+                                                                 -o tests/integration/output
