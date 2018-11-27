3fe882817934aa68492ab0a374a32413d54369df
Sam Gamble <hexadessa@gmail.com>
2018-11-27 17:01:37 +0000

    Swap 'refresh_token/' and 'access_token/'

--- src/server/oasisapi/auth/urls.py
+++ src/server/oasisapi/auth/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    url(r'^refresh_token/$', TokenObtainPairView.as_view(), name='refresh_token'),
-    url(r'^access_token/$', TokenRefreshView.as_view(), name='access_token'),
+    url(r'^refresh_token/$', TokenRefreshView.as_view(), name='refresh_token'),
+    url(r'^access_token/$', TokenObtainPairView.as_view(), name='access_token'),
 ]
