810f53bfe01d93344e817c1023029b35082f4a54
Sam Gamble <hexadessa@gmail.com>
2018-12-17 16:31:34 +0000

    Update analyses to show null for non-exisitant files

--- src/server/oasisapi/analyses/serializers.py
+++ src/server/oasisapi/analyses/serializers.py
@@ -28,13 +28,49 @@ class AnalysisSerializer(serializers.ModelSerializer):
         rep = super(AnalysisSerializer, self).to_representation(instance)
 
         request = self.context.get('request')
-        rep['input_file'] = instance.get_absolute_input_file_url(request=request)
-        rep['settings_file'] = instance.get_absolute_settings_file_url(request=request)
-        rep['input_errors_file'] = instance.get_absolute_input_errors_file_url(request=request)
-        rep['input_generation_traceback_file'] = instance.get_absolute_input_generation_traceback_file_url(request=request)
-        rep['output_file'] = instance.get_absolute_output_file_url(request=request)
-        rep['run_traceback_file'] = instance.get_absolute_run_traceback_file_url(request=request)
+        rep['input_file'] = instance.get_absolute_input_file_url(request=request) if instance.input_file else None
+        rep['settings_file'] = instance.get_absolute_settings_file_url(request=request) if instance.settings_file else None
+        rep['input_errors_file'] = instance.get_absolute_input_errors_file_url(request=request) if instance.input_errors_file else None
+        rep['input_generation_traceback_file'] = instance.get_absolute_input_generation_traceback_file_url(request=request) if instance.input_generation_traceback_file else None
+        rep['output_file'] = instance.get_absolute_output_file_url(request=request) if instance.output_file else None
+        rep['run_traceback_file'] = instance.get_absolute_run_traceback_file_url(request=request) if instance.run_traceback_file else None
 
+       # if instance.input_file:
+       #     rep['input_file'] = {
+       #         "uri": instance.get_absolute_input_file_url(request=request), 
+       #         "name": instance.input_file.filename,
+       #         "stored": str(instance.input_file.file)
+       #     }
+       # if instance.settings_file:
+       #     rep['settings_file'] = {
+       #         "uri": instance.get_absolute_settings_file_url(request=request), 
+       #         "name": instance.settings_file.filename,
+       #         "stored": str(instance.settings_file.file)
+       #     }
+       # if instance.input_errors_file:
+       #     rep['input_errors_file'] = {
+       #         "uri": instance.get_absolute_input_errors_file_url(request=request), 
+       #         "name": instance.input_errors_file.filename,
+       #         "stored": str(instance.input_errors_file.file)
+       #     }
+       # if instance.input_generation_traceback_file:
+       #     rep['input_generation_traceback_file'] = {
+       #         "uri": instance.get_absolute_input_generation_traceback_file_url(request=request), 
+       #         "name": instance.input_generation_traceback_file.filename,
+       #         "stored": str(instance.input_generation_traceback_file.file)
+       #     }
+       # if instance.output_file:
+       #     rep['output_file'] = {
+       #         "uri": instance.get_absolute_output_file_url(request=request), 
+       #         "name": instance.output_file.filename,
+       #         "stored": str(instance.output_file.file)
+       #     }
+       # if instance.run_traceback_file:
+       #     rep['run_traceback_file'] = {
+       #         "uri": instance.get_absolute_run_traceback_file_url(request=request), 
+       #         "name": instance.run_traceback_file.filename,
+       #         "stored": str(instance.run_traceback_file.file)
+       #     }
         return rep
 
 
