8e4058da75167b966798196b5c9de64fa075ed80
Sam Gamble <hexadessa@gmail.com>
2019-01-09 16:58:26 +0000

    Fix Env Prefix for new api wait script

--- src/startup_server.sh
+++ src/startup_server.sh
@@ -4,9 +4,9 @@ set -e
 # Set the ini file path 
 export OASIS_API_INI_PATH="./conf.ini"
 
-wait-for-it ${OASIS_API_SERVER_DB_HOST}:${OASIS_API_SERVER_DB_PORT} -t 60
-wait-for-it ${OASIS_API_CELERY_DB_HOST}:${OASIS_API_CELERY_DB_PORT} -t 60
-wait-for-it ${OASIS_API_RABBIT_HOST}:${OASIS_API_RABBIT_PORT} -t 60
+wait-for-it ${OASIS_SERVER_DB_HOST}:${OASIS_SERVER_DB_PORT} -t 60
+wait-for-it ${OASIS_CELERY_DB_HOST}:${OASIS_CELERY_DB_PORT} -t 60
+wait-for-it ${OASIS_RABBIT_HOST}:${OASIS_RABBIT_PORT} -t 60
 
 if [ "${STARTUP_RUN_MIGRATIONS}" = true ]; then
     python manage.py migrate
