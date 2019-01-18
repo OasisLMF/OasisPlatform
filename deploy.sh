71b45f5cceb9e48f126010e2b0b27777a57dc0ed
Sam Gamble <hexadessa@gmail.com>
2019-01-18 16:06:25 +0000

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ set -eux
 python manage.py makemigrations
 python manage.py migrate
 
-export OASIS_MODEL_DATA_DIR=/home/sam/repos/models/piwind
+#export OASIS_MODEL_DATA_DIR=<..path to piwind ..>
 
 docker build -f Dockerfile.oasis_api_server.base  -t coreoasis/oasis_api_base .
 docker build -f Dockerfile.oasis_api_server.mysql -t coreoasis/oasis_api_server .
 docker build -f Dockerfile.model_execution_worker -t coreoasis/model_execution_worker .
-
-#docker-compose up -d
+docker-compose up -d
