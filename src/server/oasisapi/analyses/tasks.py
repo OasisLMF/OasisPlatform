d76a0436c6ba923f896806d2ac149d9ab69b58dd
Sam Gamble <hexadessa@gmail.com>
2019-01-18 16:00:03 +0000

    Log exceptions in worker-monitor

--- src/server/oasisapi/analyses/tasks.py
+++ src/server/oasisapi/analyses/tasks.py
@@ -15,77 +15,100 @@ logger = get_task_logger(__name__)
 
 @celery_app.task(name='run_analysis_success')
 def run_analysis_success(output_location, analysis_pk, initiator_pk):
-    from .models import Analysis
+    logger.info('output_location: {}, analysis_pk: {}, initiator_pk: {}'.format(
+                 output_location, analysis_pk, initiator_pk))
+        
+    try:    
+        from .models import Analysis
 
-    analysis = Analysis.objects.get(pk=analysis_pk)
-    analysis.status = Analysis.status_choices.RUN_COMPLETED
+        analysis = Analysis.objects.get(pk=analysis_pk)
+        analysis.status = Analysis.status_choices.RUN_COMPLETED
 
-    analysis.output_file = RelatedFile.objects.create(
-        file=str(output_location),
-        filename=str(output_location),
-        content_type='application/gzip',
-        creator=get_user_model().objects.get(pk=initiator_pk),
-    )
+        analysis.output_file = RelatedFile.objects.create(
+            file=str(output_location),
+            filename=str(output_location),
+            content_type='application/gzip',
+            creator=get_user_model().objects.get(pk=initiator_pk),
+        )
 
-    analysis.save()
-
+        analysis.save()
+    except Exception as e:
+        logger.exception(str(e))
+        
 
 @celery_app.task(name='record_run_analysis_failure')
 def record_run_analysis_failure(analysis_pk, initiator_pk, traceback):
-    from .models import Analysis
+    logger.info('analysis_pk: {}, initiator_pk: {}, traceback: {}'.format(
+                 analysis_pk, initiator_pk, traceback))
 
-    analysis = Analysis.objects.get(pk=analysis_pk)
-    analysis.status = Analysis.status_choices.RUN_ERROR
+    try:
+        from .models import Analysis
 
-    random_filename = '{}.txt'.format(uuid.uuid4().hex)
-    analysis.run_traceback_file = RelatedFile.objects.create(
-        file=File(StringIO(traceback), name=random_filename),
-        filename=random_filename,
-        content_type='text/plain',
-        creator=get_user_model().objects.get(pk=initiator_pk),
-    )
+        analysis = Analysis.objects.get(pk=analysis_pk)
+        analysis.status = Analysis.status_choices.RUN_ERROR
 
-    analysis.save()
+        random_filename = '{}.txt'.format(uuid.uuid4().hex)
+        analysis.run_traceback_file = RelatedFile.objects.create(
+            file=File(StringIO(traceback), name=random_filename),
+            filename=random_filename,
+            content_type='text/plain',
+            creator=get_user_model().objects.get(pk=initiator_pk),
+        )
+
+        analysis.save()
+    except Exception as e:
+        logger.exception(str(e))
 
 
 @celery_app.task(name='generate_input_success')
 def generate_input_success(result, analysis_pk, initiator_pk):
-    from .models import Analysis
-    input_location, errors_location = result
+    logger.info('result: {}, analysis_pk: {}, initiator_pk: {}'.format(
+                 result, analysis_pk, initiator_pk))
+    try:
 
-    analysis = Analysis.objects.get(pk=analysis_pk)
-    analysis.status = Analysis.status_choices.READY
+        from .models import Analysis
+        input_location, errors_location = result
 
-    analysis.input_file = RelatedFile.objects.create(
-        file=str(input_location),
-        filename=str(input_location),
-        content_type='application/gzip',
-        creator=get_user_model().objects.get(pk=initiator_pk),
-    )
+        analysis = Analysis.objects.get(pk=analysis_pk)
+        analysis.status = Analysis.status_choices.READY
 
-    analysis.input_errors_file = RelatedFile.objects.create(
-        file=str(errors_location),
-        filename=str(errors_location),
-        content_type='text/csv',
-        creator=get_user_model().objects.get(pk=initiator_pk),
-    )
+        analysis.input_file = RelatedFile.objects.create(
+            file=str(input_location),
+            filename=str(input_location),
+            content_type='application/gzip',
+            creator=get_user_model().objects.get(pk=initiator_pk),
+        )
 
-    analysis.save()
+        analysis.input_errors_file = RelatedFile.objects.create(
+            file=str(errors_location),
+            filename=str(errors_location),
+            content_type='text/csv',
+            creator=get_user_model().objects.get(pk=initiator_pk),
+        )
+
+        analysis.save()
+    except Exception as e:
+        logger.exception(str(e))
     
 
 @celery_app.task(name='record_generate_input_failure')
 def record_generate_input_failure(analysis_pk, initiator_pk, traceback):
-    from .models import Analysis
+    logger.info('analysis_pk: {}, initiator_pk: {}, traceback: {}'.format(
+                 analysis_pk, initiator_pk, traceback))
+    try:
+        from .models import Analysis
 
-    analysis = Analysis.objects.get(pk=analysis_pk)
-    analysis.status = Analysis.status_choices.INPUTS_GENERATION_ERROR
+        analysis = Analysis.objects.get(pk=analysis_pk)
+        analysis.status = Analysis.status_choices.INPUTS_GENERATION_ERROR
 
-    random_filename = '{}.txt'.format(uuid.uuid4().hex)
-    analysis.input_generation_traceback_file = RelatedFile.objects.create(
-        file=File(StringIO(traceback), name=random_filename),
-        filename=random_filename,
-        content_type='text/plain',
-        creator=get_user_model().objects.get(pk=initiator_pk),
-    )
+        random_filename = '{}.txt'.format(uuid.uuid4().hex)
+        analysis.input_generation_traceback_file = RelatedFile.objects.create(
+            file=File(StringIO(traceback), name=random_filename),
+            filename=random_filename,
+            content_type='text/plain',
+            creator=get_user_model().objects.get(pk=initiator_pk),
+        )
 
-    analysis.save()
+        analysis.save()
+    except Exception as e:
+        logger.exception(str(e))
