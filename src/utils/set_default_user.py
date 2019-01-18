f23123f423dbc9ddf82a1619727e582c8d60dd3d
Sam Gamble <hexadessa@gmail.com>
2019-01-18 16:00:26 +0000

    remove exit from add default admin script

--- src/utils/set_default_user.py
+++ src/utils/set_default_user.py
@@ -10,13 +10,13 @@ from django.contrib.auth.models import User
 try:
     env_username = os.environ['OASIS_ADMIN_USER']
     env_password = os.environ['OASIS_ADMIN_PASS']
+    try:
+        u = User.objects.get(username=env_username)
+        u.set_password(env_password)
+        u.save()
+    except Exception:
+        User.objects.create_superuser(env_username, 'admin@example.com', env_password)
+
 except KeyError:
     print('User Enviroment vars not set')
-    sys.exit(1)
 
-try:
-    u = User.objects.get(username=env_username)
-    u.set_password(env_password)
-    u.save()
-except Exception:
-    User.objects.create_superuser(env_username, 'admin@example.com', env_password)
