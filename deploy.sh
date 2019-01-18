c5c1aa4e0b1b6dd384f4e144aa0c41609292bf91
Sam Gamble <hexadessa@gmail.com>
2019-01-18 16:01:26 +0000

    Fix env prefix issues "OASIS_" not "OASIS_API_"

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ set -eux
 python manage.py makemigrations
 python manage.py migrate
 
-docker build -f Dockerfile.oasis_api_server.base -t oasis_api_server:base .
-docker build -f Dockerfile.oasis_api_server.mysql -t oasis_api_server:mysql .
-docker build -f Dockerfile.model_execution_worker -t model_execution_worker .
+export OASIS_MODEL_DATA_DIR=/home/sam/repos/models/piwind
 
-docker-compose up -d
+docker build -f Dockerfile.oasis_api_server.base  -t coreoasis/oasis_api_base .
+docker build -f Dockerfile.oasis_api_server.mysql -t coreoasis/oasis_api_server .
+docker build -f Dockerfile.model_execution_worker -t coreoasis/model_execution_worker .
+
+#docker-compose up -d
