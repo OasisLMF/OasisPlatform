59f1b142c3b780e563bedc8a429c837ee5add337
Sam Gamble <hexadessa@gmail.com>
2019-02-13 15:50:49 +0000

    Add more sleeps to startup_tester

--- src/startup_tester.sh
+++ src/startup_tester.sh
@@ -7,12 +7,12 @@ host=server:8000
 SERVER_HEALTH=0
 until [ $SERVER_HEALTH -gt 0 ] ; do
   >&2 echo "Server is unavailable - sleeping"
-  sleep 1
   echo "curl -X GET 'http://$host/healthcheck/'"
-  SERVER_HEALTH=$(curl -X GET "http://$host/healthcheck/" -H "accept: application/json" | grep -c "OK") > /dev/null
+  SERVER_HEALTH=$(curl -X GET "http://$host/healthcheck/" -H "accept: application/json" | grep -c "OK") 2>/dev/null
+  sleep 5
 done
 echo "Server is available - exit"
-
+sleep 10
 set -e
 TIMEOUT=180
 timeout $TIMEOUT pytest -v -p no:django /home/worker/tests/integration/api_integration.py "$@"
