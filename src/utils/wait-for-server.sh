287eb5a1dca0ceca30b3a7eb7c5bed2abbc58f41
Sam Gamble <hexadessa@gmail.com>
2019-02-13 13:57:03 +0000

    Add wait for server to monitor-worker

---
+++ src/utils/wait-for-server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# wait-for-server.sh
+
+host="$1"
+shift
+cmd="$@"
+
+
+
+SERVER_HEALTH=0
+until [ $SERVER_HEALTH -gt 0 ] ; do
+  >&2 echo "Server is unavailable - sleeping"
+  sleep 1
+  echo "curl -X GET 'http://$host/healthcheck/'"
+  SERVER_HEALTH=$(curl -X GET "http://$host/healthcheck/" -H "accept: application/json" | grep -c "OK")
+done
+
+>&2 echo "Server is up - executing command: " 
+echo $cmd
+exec $cmd
