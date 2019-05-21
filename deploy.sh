6e94f00338fdb7808f2cea2db874d3f1ad6f6f64
Sam Gamble <hexadessa@gmail.com>
2019-05-21 12:24:45 +0100

    clear latest images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#export OASIS_MODEL_DATA_DIR=<..path to piwind ..>
+export OASIS_MODEL_DATA_DIR=<..path to piwind ..>
+
+docker rmi coreoasis/api_base:latest
+docker rmi coreoasis/api_server:latest
+docker rmi coreoasis/model_worker:latest
 
 docker build -f Dockerfile.api_server.base  -t coreoasis/api_base .
 docker build -f Dockerfile.api_server.mysql -t coreoasis/api_server .
