c7f3fd1f8d6a2ffce6e3b9dfb578690e29a99a1f
Sam Gamble <hexadessa@gmail.com>
2019-07-05 17:44:29 +0100

    Remove combine api_server docker files into one

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,9 @@
 
 export OASIS_MODEL_DATA_DIR=<..path to piwind ..>
 
-docker rmi coreoasis/api_base:latest
 docker rmi coreoasis/api_server:latest
 docker rmi coreoasis/model_worker:latest
 
-docker build -f Dockerfile.api_server.base  -t coreoasis/api_base .
-docker build -f Dockerfile.api_server.mysql -t coreoasis/api_server .
+docker build -f Dockerfile.api_server -t coreoasis/api_server .
 docker build -f Dockerfile.model_worker -t coreoasis/model_worker .
 docker-compose up -d
