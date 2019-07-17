aa8ebeb12b12cc681faca4651ed8e11c033aa38b
Sam Gamble <hexadessa@gmail.com>
2019-07-17 16:35:16 +0100

    Add OED column defs for oasislmf

--- src/server/oasisapi/oed_info/views.py
+++ src/server/oasisapi/oed_info/views.py
@@ -1,8 +1,16 @@
 from rest_framework import views
 from rest_framework.response import Response
+
 from oasislmf import __version__ as oasislmf_package_ver
 from oasislmf.utils.peril import PERIL_GROUPS, PERILS
-
+from oasislmf.utils.summary_levels import (
+    'SUMMARY_LEVEL_LOC',
+    'SUMMARY_LEVEL_ACC',
+    'OED_LOCATION_COLS',
+    'OED_ACCOUNT_COLS',
+    'OED_REINSINFO_COLS',
+    'OED_REINSSCOPE_COLS',
+)
 
 class PerilcodesView(views.APIView):
     """
@@ -19,13 +27,34 @@ class PerilcodesView(views.APIView):
                                                 'peril_ids': PERIL_GROUPS[g]['peril_ids']
                                                 }  for g in PERIL_GROUPS.keys()}
 
-        return Response({'oasislmf_version': oasislmf_package_ver,
-                        'peril_codes': peril_codes,
-                        'peril_groups': peril_groups})
+        return Response({
+            'oasislmf_version': oasislmf_package_ver,
+            'peril_codes': peril_codes,
+            'peril_groups': peril_groups
+        })
 
-class SummaryLevelsView(views.APIView)
+class SummaryLevelsView(views.APIView):
     """
-    Return a list of all support OED columns for summary_level selection
+    Return a list of recommended OED columns for summary_level selection
     """
     def get(self, request):
-        return Response({})
+        loc_summary_lvl = {k: v for k, v in OED_LOCATION_COLS.items() if k.lower() in SUMMARY_LEVEL_LOC}
+        acc_summary_lvl = {k: v for k, v in OED_ACCOUNT_COLS.items() if k.lower() in SUMMARY_LEVEL_ACC}
+        return Response({
+            'oasislmf_version': oasislmf_package_ver,
+            'location_file': loc_summary_lvl,
+            'accounts_file': acc_summary_lvl,
+        })
+
+class OedColsView(views.APIView):
+    """
+    Return a list of all OED columns 
+    """
+    def get(self, request):
+        return Response({
+            'oasislmf_version': oasislmf_package_ver,
+            'location_file': OED_LOCATION_COLS,
+            'accounts_file': OED_ACCOUNT_COLS,
+            'reinsurance_info_file': OED_REINSINFO_COLS,
+            'reinsurance_scope_file': OED_REINSSCOPE_COLS,
+        })
