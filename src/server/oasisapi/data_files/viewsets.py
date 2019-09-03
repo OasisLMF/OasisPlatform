57b5203436b734d05e54188f69c5658e0a66f1b2
Sam Gamble <hexadessa@gmail.com>
2019-09-03 14:13:10 +0100

    Fix file responses

--- src/server/oasisapi/data_files/viewsets.py
+++ src/server/oasisapi/data_files/viewsets.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django_filters import rest_framework as filters
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
@@ -9,6 +10,7 @@ from ..files.serializers import RelatedFileSerializer
 from ..files.views import handle_related_file
 from ..filters import TimeStampedFilter
 from .models import DataFile
+from ..schemas import FILE_RESPONSE
 from .serializers import DataFileSerializer
 
 
@@ -78,6 +80,7 @@ class DataFileViewset(viewsets.ModelViewSet):
         else:
             return api_settings.DEFAULT_PARSER_CLASSES
 
+    @swagger_auto_schema(methods=['get'], responses={200: FILE_RESPONSE})
     @action(methods=['get', 'post', 'delete'], detail=True)
     def content(self, request, pk=None, version=None):
         """
