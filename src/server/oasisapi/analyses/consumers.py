1706912525b6b7ad948cde326d22ec25ced07107
Dan Bate <dan.bate@gmail.com>
2020-02-07 18:28:26 +0000

    added models to the web socket notifications

--- src/server/oasisapi/analyses/consumers.py
+++ src/server/oasisapi/analyses/consumers.py
@@ -9,12 +9,14 @@ from src.server.oasisapi.queues.serializers import QueueSerializer
 
 def build_task_status_message(analysis, tasks, queues):
     from src.server.oasisapi.analyses.serializers import AnalysisSerializer, AnalysisTaskStatusSerializer
+    from src.server.oasisapi.analysis_models.serializers import AnalysisModelSerializer
 
     content = {
         'time': DateTimeField().to_representation(now()),
         'type': 'analysis_task_status.updated',
         'content': {
             'analysis': AnalysisSerializer(instance=analysis, include_task_statuses=False).to_representation(analysis),
+            'model': AnalysisModelSerializer(instance=analysis.model).to_representation(analysis.model),
             'tasks': AnalysisTaskStatusSerializer(many=True, instance=tasks).to_representation(tasks),
             'queues': QueueSerializer(many=True, instance=queues).to_representation(queues),
         }
