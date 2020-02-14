13e938439073b89a84f31c65a999c7f42d220cff
Sam Gamble <hexadessa@gmail.com>
2020-02-14 15:47:13 +0000

    First pass a storage manager

--- src/model_execution_worker/storage_manager.py
+++ src/model_execution_worker/storage_manager.py
@@ -1,49 +1,115 @@
+import re
+import os
+import wget
+import tarfile
+import uuid
+import shutil
 
-class StorageManager(object):
-    """ Instantiate a storage connector based on workers config
+from os import path
+from oasislmf.utils.exceptions import OasisException
+
+LOG_FILE_SUFFIX = '.txt'
+ARCHIVE_FILE_SUFFIX = '.tar'
+
+
+#class StorageManager(object):
+#    """ Instantiate a storage connector based on workers config
+#    """
+#    def __init__(self, settings_conf):
+#        self.storage_conf = read_conf(storage_conf)
+#
+#        if storage_type == '<S3 enum>':
+#            return AwsObjectStore( ... )
+#        #elif storage_type == '<azure enum>':
+#        #    return AzureObjectStore( ... )
+#        else:
+#            return BaseStorageConnector( ... )
+
+class MissingInputsException(OasisException):
+    def __init__(self, input_filepath):
+        super(MissingInputsException, self).__init__('Input file not found: {}'.format(input_filepath))
+
+class BaseStorageConnector(object):
+    """ Base class to implement a storage service
+
     """
-    def __init__(self, storage_conf):
-        self.storage_conf = read_conf(storage_conf)
+    def __init__(self, worker_conf):
+        self.settings = worker_conf
+        self.media_root = self.settings.get('worker', 'MEDIA_ROOT')
 
-        if storage_type == '<S3 enum>':
-            return AwsObjectStore( ... )
-        #elif storage_type == '<azure enum>':
-        #    return AzureObjectStore( ... )
+    def _get_unique_filename(self, suffix=""):
+       return "{}.{}".format(uuid.uuid4().hex, suffix)
+
+    def _is_valid_url(self, url):
+       # Replace this later?
+        regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return re.match(regex, url) is not None
+
+    def extract(self, archive_fp, directory):
+        with tarfile.open(archive_fp) as f:
+            f.extractall(directory)
+
+    def compress(self, archive_fp, directory, arcname='/'):
+        with tarfile.open(archive_fp, 'w:gz') as tar:
+            tar.add(directory, arcname=arcname)
+
+    def get(self, reference, download_dir=None):
+        """
+            Top level 'get from storage' function
+
+            Case m: 'reference' is a URL to a remote file, download
+                    to `dest` location if set or CWD if None
+
+            Case n: 'reference' is a valid path to a shared volumne file
+                    copy to 'dest' if set to a valid filepath
+
+                    return sf-share + filename
+        """
+        if self._is_valid_url(reference):
+            return wget.download(
+                url=reference, 
+                out=os.path.abspath(download_dir)
+        elif isinstance(reference, str):
+            filepath = os.path.join(
+                self.media_root,
+                os.path.basename(reference)
+            )
+            if os.path.isfile(filepath):
+                return filepath
+            else:
+                raise MissingInputsException(filepath)
+
         else:
-            return FileSystemShare( ... )
+            return None
+
+    def put(self, reference, suffix="", arcname=None):
+        """ Top level send to storage function
+        """
+        ext = os.path.splitext(reference)[-1] if not suffix else suffix
+        stored_fp = os.path.join(
+            self.media_root,
+            self._get_unique_filename(ext)
+        )
+
+        if os.path.isfile(reference):
+            return shutil.copy(reference, stored_fp)
+        elif os.path.isdir(reference):
+            self.compress(stored_fp, reference, arcname)
+            return stored_fp
+        else:
+            return None
 
 
-class StorageConnector(object)
-    """ Base interface class to implement a storage service
-    """
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
-
-    def _extract(self):
-        pass
-
-    def _compress(self):
+class AwsObjectStore(BaseStorageConnector):
+    def __init__(self, conf_location):
         pass
 
     def connect(self, *args, **kwargs):
         raise NotImplementedError
-
-    def fetch(self, reference, dest=None):
-        raise NotImplementedError
-
-    def store_dir(self, dir_reference):
-        raise NotImplementedError
-
-    def store_file(self, file_reference):
-        raise NotImplementedError
-
-
-class FileSystemShare(StorageConnector):
-    def __init__(self, fileshare_dir):
-        pass
-
-class AwsObjectStore(BaseConnector):
-    def __init__(self, conf_location):
-        pass
-
 
