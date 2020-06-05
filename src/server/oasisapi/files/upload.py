2089c3239212ff3e4ae5742d8210e992f125ea9d
Sam Gamble <hexadessa@gmail.com>
2020-06-05 09:17:02 +0100

    Fix compressed tar for S3

--- src/server/oasisapi/files/upload.py
+++ src/server/oasisapi/files/upload.py
@@ -3,5 +3,9 @@ from uuid import uuid4
 
 
 def random_file_name(instance, filename):
-    ext = os.path.splitext(filename)[-1]
+    # Work around: S3 objects pushed as '<hash>.gz' should be '<hash>.tar.gz'
+    if filename.endswith('.tar.gz'):
+        ext = '.tar.gz'
+    else:
+        ext = os.path.splitext(filename)[-1]
     return '{}{}'.format(uuid4().hex, ext)
