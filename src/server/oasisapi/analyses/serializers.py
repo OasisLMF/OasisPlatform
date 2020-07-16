3e74043240730ebf056e415afc4419decae713c3
Sam Gamble <hexadessa@gmail.com>
2020-07-16 13:17:12 +0100

    #375 - Add check for non-null location files to analyses validation

--- src/server/oasisapi/analyses/serializers.py
+++ src/server/oasisapi/analyses/serializers.py
@@ -103,6 +103,10 @@ class AnalysisSerializer(serializers.ModelSerializer):
         if not attrs.get('creator') and 'request' in self.context:
             attrs['creator'] = self.context.get('request').user
 
+        # Check that portfilio has a location file
+        if not attrs['portfolio'].location_file:
+            raise ValidationError({'portfolio': '"location_file" must not be null'})
+
         # check that model isn't soft-deleted
         if hasattr(attrs, 'model'):    
             if attrs['model'].deleted:
