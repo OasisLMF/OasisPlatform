0b82171061e995d0354d481a0fcb736f72002b35
Sam Gamble <hexadessa@gmail.com>
2021-06-18 15:16:39 +0100

    Fix missing PiWind image

--- jenkins/oasis_platform.groovy
+++ jenkins/oasis_platform.groovy
@@ -219,7 +219,7 @@ node {
                     sh "docker build -f docker/Dockerfile.release-notes -t release-builder ."
                 }
             }
-        }    
+        }
 
         if (params.SCAN_IMAGE_VULNERABILITIES.replaceAll(" \\s","")){
             parallel(
@@ -265,14 +265,18 @@ node {
                 }
             }
         }
-       if (params.CHECK_COMPATIBILITY) {
 
-            // Build PiWind worker from new worker
-            stage('Build: PiWind worker') {
-                dir(model_workspace) {
-                    sh "docker build --build-arg worker_ver=${env.TAG_RELEASE} -f ${docker_piwind} -t ${image_piwind}:${env.TAG_RELEASE} ."
-                }
-            }
+        if (params.CHECK_S3 || params.CHECK_COMPATIBILITY) {
+              // Build PiWind worker from new worker
+              stage('Build: PiWind worker') {
+                  dir(model_workspace) {
+                      sh "docker build --build-arg worker_ver=${env.TAG_RELEASE} -f ${docker_piwind} -t ${image_piwind}:${env.TAG_RELEASE} ."
+                  }
+              }
+
+         }
+
+         if (params.CHECK_COMPATIBILITY) {
 
             // START API for base model tests
             stage('Run: API Server') {
