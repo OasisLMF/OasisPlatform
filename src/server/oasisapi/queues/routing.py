a85d0ee86629ed41831bd8156345a7cb6994bf28
Sam Gamble <hexadessa@gmail.com>
2021-08-23 10:05:41 +0100

    Fix websocket

--- src/server/oasisapi/queues/routing.py
+++ src/server/oasisapi/queues/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from src.server.oasisapi.queues import consumers
 
 websocket_urlpatterns = [
-    path('v1/queue-status/', consumers.QueueStatusConsumer),
+    path('v1/queue-status/', consumers.QueueStatusConsumer.as_asgi()),
 ]
