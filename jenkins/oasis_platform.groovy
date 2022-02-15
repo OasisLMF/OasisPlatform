8b9b9d65e7e0a680096154c7fde5a999ceb25652
Sam Gamble <hexadessa@gmail.com>
2022-02-15 10:33:35 +0000

    test vs the last release

--- jenkins/oasis_platform.groovy
+++ jenkins/oasis_platform.groovy
@@ -194,7 +194,7 @@ node {
                     if (params.PREV_RELEASE_TAG){
                         env.LAST_RELEASE_TAG = params.PREV_RELEASE_TAG
                     } else {
-                        sh "curl https://api.github.com/repos/OasisLMF/OasisPlatform/tags | jq -r '( first ) | .name' > last_release_tag"
+                        sh "curl https://api.github.com/repos/OasisLMF/OasisPlatform/releases | jq -r '( first ) | .name' > last_release_tag"
                         env.LAST_RELEASE_TAG = readFile('last_release_tag').trim()
                     }
                     println("LAST_RELEASE = $env.LAST_RELEASE_TAG")
