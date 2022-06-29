e2ca1e82b034dbbfcddec2ee56c66246fefc1d11
Sam Gamble <hexadessa@gmail.com>
2022-06-29 10:03:16 +0100

    Run update requirments as root

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Helper script to update python requirment files within the docker container
 docker build --build-arg DOCKER_USER=$(whoami) -f docker/Dockerfile.update_python_req -t coreoasis/platform_req:update .
-# docker run -it --user $(id -u):$(id -g) -v $PWD:/mnt -w /mnt coreoasis/platform_req:update /bin/bash
+
+# run as mapped user 
+#USER="$(id -u):$(id -g)"
+
+# run as root 
+USER='root'
 
 echo 'Update requirements-server.txt'
-docker run --user $(id -u):$(id -g) -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-server.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-server.in"
 echo 'Update requirements-worker.txt'
-docker run --user $(id -u):$(id -g) -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-worker.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-worker.in"
 echo 'Update requirements.txt'
-docker run --user $(id -u):$(id -g) -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements.in"
