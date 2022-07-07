a90eade8b22e959f64c99e376736e49e0bbd9a7f
sambles <sambles@users.noreply.github.com>
2022-07-07 16:27:47 +0100

    Fix/CVE issues 1.26.1 (#667)
    * CVE-2021-34141 CVE-2022-34265
    
    * Update requ script and files
    
    * Numpy
    
    * Ignore secret check on images

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Helper script to update python requirment files within the docker container 
 
-docker build -f docker/Dockerfile.update_python_req -t coreoasis/platform_req:update .
+docker build --build-arg DOCKER_USER=$(whoami) -f docker/Dockerfile.update_python_req -t coreoasis/platform_req:update .
+
+# run as mapped user 
+USER="$(id -u):$(id -g)"
+
+# run as root 
+#USER='root'
 
 echo 'Update requirements-server.txt'
-docker run -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-server.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-server.in"
 
 echo 'Update requirements-worker.txt'
-docker run -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-worker.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements-worker.in"
 
 echo 'Update requirements.txt'
-docker run -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements.in"
+docker run --user $USER -v $PWD:/mnt -w /mnt coreoasis/platform_req:update  /bin/sh -c "pip-compile requirements.in"
