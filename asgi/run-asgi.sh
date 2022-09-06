91873c24bb25d06df83e7193450b2bc8faf0be5e
sambles <sambles@users.noreply.github.com>
2022-09-06 14:48:13 +0100

    Fix large file download/upload timeout  (#685)
    * Scrap all changes and lay groundwork for two server containers
    
    Switch socket port to 8001
    
    Add uwsgi to requ
    
    switch default command to wsgi
    
    Revert "Add uwsgi to requ"
    
    This reverts commit ae7f1081931392f9ecdce6fc25eac53fca0a1e4d.
    
    Install uwsgi as binary package
    
    Revert "Revert "Add uwsgi to requ""
    
    This reverts commit 2f82ddf6fb0a7546bda5e8d4391832d12c36697f.
    
    Add wsgi
    
    Revert "Revert "Revert "Add uwsgi to requ"""
    
    This reverts commit c43f2b34bf939bf8aba21c21fefd4f6d92e1e505.
    
    try gunicorn
    
    fix routing
    
    Increase timeout and match worker count to system cores
    
    Increased timeout fixed download
    
    test charts
    
    Fix API azure access
    
    Fixed DNS routing to websocket container
    
    Fix token access issue?
    
    Fix sub_path enable/disable
    
    Disable "/api/ sub-path for internal web-socket server"
    
    websocket server has no connection to keycloak for auth, try using the current oasisServer options
    
    Fix?
    
    fix
    
    ugh
    
    shot in the dark
    
    Remove added client
    
    use keycloak settings from server values
    
    * Add option for ssl redis (#687)
    
    * Add option for ssl redis
    
    * Update configMaps
    
    * syntax fix
    
    * Add missing ssl data in configMap
    
    * FIx ssl data in ConfigMap
    
    * When running with REST api in Subpath, also work with root pathing

--- asgi/run-asgi.sh
+++ asgi/run-asgi.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-daphne -b 0.0.0.0 -p 8000 --root-path /api src.server.oasisapi.asgi:application
+#daphne -b 0.0.0.0 -p 8001 --root-path /api src.server.oasisapi.asgi:application
+daphne -b 0.0.0.0 -p 8001 src.server.oasisapi.asgi:application
