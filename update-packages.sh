b91d33e74b0ef94457c63ae726b6328c22f0c13c
Sam Gamble <hexadessa@gmail.com>
2022-10-20 15:10:04 +0100

    Add pip-compile scirpt

---
+++ update-packages.sh
@@ -0,0 +1,34 @@
+pkg_list=(
+    ods-tools
+    oasislmf
+    'django==3.*'
+    'celery==5.*'
+    virtualenv
+    filelock
+    text-unidecode
+    azure-storage-blob
+    coverage
+    django-request-logging
+    drf-yasg
+    numpy
+    scipy
+    waitress
+    sklearn
+    psycopg2-binary
+    scikit-learn
+)
+
+
+for pk in "${pkg_list[@]}"; do
+    pip-compile --upgrade-package $pk requirements-worker.in
+    pip-compile --upgrade-package $pk requirements-server.in
+    pip-compile --upgrade-package $pk requirements.in
+
+    if [[ `git status --porcelain --untracked-files=no` ]]; then
+        echo "$pk - updated"
+        git add -u
+        git commit -m "Updated package $pk"
+    else 
+        echo "$pk - no update found"
+    fi     
+done 
