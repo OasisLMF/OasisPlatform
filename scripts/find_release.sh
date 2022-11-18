efb469146f927d8712e144ebfb03351254717ded
Sam Gamble <hexadessa@gmail.com>
2022-11-18 13:19:21 +0000

    Fix for ktools tags which start with "v"

--- scripts/find_release.sh
+++ scripts/find_release.sh
@@ -4,6 +4,7 @@
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 pre_release='false'
 tag_select='1'
+version_prefix=''
 
 print_usage() {
       echo "find_release.sh - Find release tags from repo"
@@ -15,29 +16,26 @@ print_usage() {
       echo " -b=<branch_name>     branch to search for tags"
       echo " -p=<true|false>      Ignore tags marked with '{n}.{n}.{n}rc{n}'"
       echo " -t=<int>             Select tag from most recent to least '1 = most recent by date'"
+      echo " -v=<string>          Version tag prefix, e.g. ktools needs 'v' for tag format v{n}.{n}.{n}"
       exit 0
 }
 
-while getopts 'b:p:t:' flag; do
+while getopts 'b:p:t:v:' flag; do
   case "${flag}" in
     b) branch_name=${OPTARG} ;;
     p) pre_release=${OPTARG} ;;
     t) tag_select=${OPTARG} ;;
+    t) version_prefix=${OPTARG} ;;
     *) print_usage
        exit 1 ;;
   esac
 done
 
-
-
-
-
 if [[ "$pre_release" = "false" ]]; then
-    release_tags=( $(git tag --merged $branch_name --sort=creatordate | grep -oP "^(\d+)\.(\d+)\.(\d+)$") )
+    release_tags=( $(git tag --merged $branch_name --sort=creatordate | grep -oP "^$version_prefix(\d+)\.(\d+)\.(\d+)$") )
 else
-    release_tags=( $(git tag --merged $branch_name --sort=creatordate | grep -oP "^(\d+)\.(\d+)\.(\d+)$|^(\d+)\.(\d+)\.(\d+)rc(\d+)") )
+    release_tags=( $(git tag --merged $branch_name --sort=creatordate | grep -oP "^$version_prefix(\d+)\.(\d+)\.(\d+)$|^(\d+)\.(\d+)\.(\d+)rc(\d+)") )
 fi
-
 
 # --  DEBUG inputs ----------
 #echo "${release_tags[*]}"
