ebecb93f5231b9a41da7858736370056ee58f436
Sam Gamble <hexadessa@gmail.com>
2023-02-02 13:24:40 +0000

    Add retry for failed model reg tasks

--- src/server/oasisapi/celery_app.py
+++ src/server/oasisapi/celery_app.py
@@ -10,3 +10,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.server.oasisapi.settings')
 celery_app = Celery('oasisapi')
 celery_app.config_from_object('django.conf:settings')
 celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+server_task_kwargs = {
+    'autoretry_for': (Exception,),
+    'max_retries': 2,               # The task will be run max_retries + 1 times
+    'default_retry_delay': 5,       # A small delay to recover from temporary bad states
+}
