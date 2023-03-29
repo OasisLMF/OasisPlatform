08942d1efd0363a11c02b5e2ca57a947e05ddcb0
Sam Gamble <hexadessa@gmail.com>
2023-03-29 14:42:58 +0100

    Add unit tests for gen_and_run

--- src/server/oasisapi/analyses/tests/test_analysis_model.py
+++ src/server/oasisapi/analyses/tests/test_analysis_model.py
@@ -17,6 +17,8 @@ from ..models import Analysis
 from ..tasks import record_run_analysis_result, record_generate_input_result
 from .fakes import fake_analysis, FakeAsyncResultFactory
 
+from unittest.mock import MagicMock, patch
+
 # Override default deadline for all tests to 8s
 settings.register_profile("ci", deadline=800.0)
 settings.load_profile("ci")
@@ -241,3 +243,128 @@ class AnalysisGenerateInputs(WebTestMixin, TestCase):
                 self.assertEqual(sig.task, 'generate_input')
                 self.assertEqual(sig.args, (analysis.id, analysis.portfolio.location_file.file.name, None, None, None, None, []))
                 self.assertEqual(sig.options['queue'], analysis.model.queue_name)
+
+
+class AnalysisGenerateAndRun(WebTestMixin, TestCase):
+    @given(
+        status=sampled_from([
+            Analysis.status_choices.READY,
+            Analysis.status_choices.RUN_COMPLETED,
+            Analysis.status_choices.RUN_CANCELLED,
+            Analysis.status_choices.RUN_ERROR
+        ]),
+        task_gen_id=text(min_size=1, max_size=10, alphabet=string.ascii_letters),
+        task_run_id=text(min_size=1, max_size=10, alphabet=string.ascii_letters),
+    )
+    def test_state_is_ready___run_is_started(self, status, task_gen_id, task_run_id):
+        with TemporaryDirectory() as d:
+            with override_settings(MEDIA_ROOT=d):
+                initiator = fake_user()
+                analysis = fake_analysis(
+                    status=status,
+                    run_task_id=task_run_id,
+                    portfolio=fake_portfolio(location_file=fake_related_file()),
+                    settings_file=fake_related_file())
+
+                task_gen = Mock()
+                task_run = Mock()
+                task_chain = Mock()
+
+                res_factory_gen = FakeAsyncResultFactory(target_task_id=task_gen_id)
+                res_factory_run = FakeAsyncResultFactory(target_task_id=task_run_id)
+
+                task_gen.delay.return_value = res_factory_gen(task_gen_id)
+                task_run.delay.return_value = res_factory_run(task_run_id)
+                task_chain.delay.return_value = res_factory_run(task_run_id)
+
+                with (
+                    patch('src.server.oasisapi.analyses.models.Analysis.generate_input_signature', PropertyMock(return_value=task_gen)),
+                    patch('src.server.oasisapi.analyses.models.Analysis.run_analysis_chain_signature', PropertyMock(return_value=task_run)),
+                    patch('src.server.oasisapi.analyses.models.chain', PropertyMock(return_value=task_chain)) as task_chain,
+                ):
+                    analysis.generate_and_run(initiator, validate_celery=False)
+
+                    task_gen.link.assert_called_with(record_generate_input_result.s(analysis.pk, initiator.pk))
+                    task_gen.link_error.assert_called_with(
+                        signature('on_error', args=('record_generate_input_failure', analysis.pk, initiator.pk), queue=analysis.model.queue_name))
+
+                    task_run.link.assert_called_once_with(record_run_analysis_result.s(analysis.pk, initiator.pk))
+                    task_run.link_error.assert_called_with(
+                        signature('on_error', args=('record_run_analysis_failure', analysis.pk, initiator.pk), queue=analysis.model.queue_name))
+
+                    task_chain.assert_called_once_with(task_gen, task_run)
+
+    @given(
+        status=sampled_from([
+            Analysis.status_choices.INPUTS_GENERATION_QUEUED,
+            Analysis.status_choices.INPUTS_GENERATION_STARTED,
+            Analysis.status_choices.RUN_QUEUED,
+            Analysis.status_choices.RUN_STARTED,
+        ]),
+        task_id=text(min_size=1, max_size=10, alphabet=string.ascii_letters),
+    )
+    def test_state_is_running_or_generating_inputs___validation_error_is_raised_revoke_is_not_called(self, status, task_id):
+        res_factory = FakeAsyncResultFactory(target_task_id=task_id)
+        initiator = fake_user()
+
+        task_chain = Mock()
+        task_chain.delay.return_value = res_factory(task_id)
+
+        with (
+                patch('src.server.oasisapi.analyses.models.chain', PropertyMock(return_value=task_chain)) as task_chain):
+            analysis = fake_analysis(status=status, run_task_id=task_id)
+
+            with self.assertRaises(ValidationError) as ex:
+                analysis.generate_and_run(initiator, validate_celery=False)
+
+            self.maxDiff = None
+            self.assertEqual({
+                'portfolio': ['"location_file" must not be null'],
+                'settings_file': ['Must not be null'],
+                'status': ['Analysis status must be one of [NEW, INPUTS_GENERATION_ERROR, INPUTS_GENERATION_CANCELLED, READY, RUN_COMPLETED, RUN_CANCELLED, RUN_ERROR]'],
+            }, ex.exception.detail)
+
+            self.assertEqual(status, analysis.status)
+            self.assertFalse(res_factory.revoke_called)
+
+    @given(
+        status=sampled_from([
+            Analysis.status_choices.READY,
+            Analysis.status_choices.RUN_COMPLETED,
+            Analysis.status_choices.RUN_CANCELLED,
+            Analysis.status_choices.RUN_ERROR
+        ]),
+        task_id=text(min_size=1, max_size=10, alphabet=string.ascii_letters),
+    )
+    def test_worker_not_supported__validation_error_is_raised_revoke_is_not_called(self, status, task_id):
+        with TemporaryDirectory() as d:
+            with override_settings(MEDIA_ROOT=d):
+                initiator = fake_user()
+                res_factory = FakeAsyncResultFactory(target_task_id=task_id)
+                analysis = fake_analysis(
+                    status=status,
+                    run_task_id=task_id,
+                    portfolio=fake_portfolio(location_file=fake_related_file()),
+                    settings_file=fake_related_file())
+
+                task_chain = Mock()
+                task_chain.delay.return_value = res_factory(task_id)
+
+                task_celery_support = Mock()
+                task_celery_support.wait = MagicMock(return_value=(False, "celery error message"))
+
+                with (
+                    patch('src.server.oasisapi.analyses.models.chain', PropertyMock(return_value=task_chain)) as task_chain,
+                    patch('src.server.oasisapi.analyses.models.check_model_task_support.delay', PropertyMock(return_value=task_celery_support))
+                ):
+
+                    with self.assertRaises(ValidationError) as ex:
+                        analysis.generate_and_run(initiator, validate_celery=True)
+
+                    self.assertEqual([k for k in ex.exception.detail], ['worker'])
+                    self.assertEqual(
+                        str(ex.exception.detail.get('worker', '')),
+                        "Celery task 'generate_and_run' not supported in worker version None"
+                    )
+                    self.assertEqual(status, analysis.status)
+                    self.assertFalse(res_factory.revoke_called)
