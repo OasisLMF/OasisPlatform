1a182988956b7faa87a7cc45d566e371ecd4ba7e
sambles <sambles@users.noreply.github.com>
2023-03-30 10:33:19 +0100

    Fix/user data loading (#779)
    * add filepath func storage base class
    
    * replace .get with .filepath
    
    * Tidy up stray else

--- src/model_execution_worker/distributed_tasks.py
+++ src/model_execution_worker/distributed_tasks.py
@@ -1076,20 +1076,18 @@ def prepare_complex_model_file_inputs(complex_model_files, run_directory):
         stored_fn = cmf[STORED_FILENAME]
         orig_fn = cmf[ORIGINAL_FILENAME]
 
-        if filestore._is_valid_url(stored_fn):
-            # If reference is a URL, then download the file & rename to 'original_filename'
-            fpath = filestore.get(stored_fn, run_directory)
-            shutil.move(fpath, os.path.join(run_directory, orig_fn))
-        elif filestore._is_locally_stored(stored_fn):
+        if filestore._is_locally_stored(stored_fn):
             # If refrence is local filepath check that it exisits and copy/symlink
-            from_path = filestore.get(stored_fn)
+            from_path = filestore.filepath(stored_fn)
             to_path = os.path.join(run_directory, orig_fn)
             if os.name == 'nt':
                 shutil.copy(from_path, to_path)
             else:
                 os.symlink(from_path, to_path)
         else:
-            os.symlink(from_path, to_path)
+            # If reference is a remote, then download the file & rename to 'original_filename'
+            fpath = filestore.get(stored_fn, run_directory)
+            shutil.move(fpath, os.path.join(run_directory, orig_fn))
 
 
 @task_failure.connect
