3c49dcb2f6f51899aea03d5354211a1a821bb4a1
Carl Fischer <carl.fischer@jbarisk.com>
2023-05-05 10:20:47 +0100

    Fixes

--- src/startup_worker.sh
+++ src/startup_worker.sh
@@ -32,4 +32,4 @@ else
 fi
 
 # Start new worker on init
-celery --app src.model_execution_worker.distributed_tasks worker $WORKER_CONCURRENCY --loglevel=INFO -Q "${OASIS_MODEL_SUPPLIER_ID}-${OASIS_MODEL_ID}-${OASIS_MODEL_VERSION_ID}" ${EXTRA_CELERY_ARGS} |& tee -a /var/log/oasis/worker.log
+celery --app src.model_execution_worker.distributed_tasks worker $WORKER_CONCURRENCY --loglevel=INFO -Q "${OASIS_MODEL_SUPPLIER_ID}-${OASIS_MODEL_ID}-${OASIS_MODEL_VERSION_ID}" ${OASIS_CELERY_EXTRA_ARGS} |& tee -a /var/log/oasis/worker.log
