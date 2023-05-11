a56708c25ebb731ec7a813f9e748955bf08c3dfc
sambles <sambles@users.noreply.github.com>
2023-05-11 16:49:27 +0100

    Ci update 1.15 (#808)
    * Ci updates 1.23.x (#807)
    
    * Update CI scripts
    
    * Update CI scripts
    
    * revert publish script
    
    * revert changelog script
    
    * Fix minor ver to 23
    
    * blap code-quality
    
    * Update scripts
    
    * Update workflows

---
+++ scripts/update-packages.sh
@@ -0,0 +1,38 @@
+pkg_list=(
+    'oasislmf==1.15.*'
+    joblib
+    oauthlib
+    parso
+    certifi
+    cryptography
+    wheel
+    ruamel.yaml
+    distlib
+    'sqlalchemy==1.*'
+    'django==3.*'
+    django-celery-results
+    'celery==5.*'
+    virtualenv
+    filelock
+    text-unidecode
+    azure-storage-blob
+    coverage
+    django-request-logging
+    drf-yasg
+    scipy
+    waitress
+    sklearn
+    psycopg2-binary
+    scikit-learn
+)
+
+
+PKG_UPDATE=''
+for pk in "${pkg_list[@]}"; do
+    PKG_UPDATE=$PKG_UPDATE" --upgrade-package $pk"   
+done
+
+set -e 
+pip-compile $PKG_UPDATE requirements-worker.in
+pip-compile $PKG_UPDATE requirements-server.in
+pip-compile $PKG_UPDATE requirements.in
