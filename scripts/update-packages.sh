9c8a3d0db67f416a00188be40b77360098a246b8
sambles <sambles@users.noreply.github.com>
2023-05-31 11:06:32 +0100

    Add option to select packages to update in workflow (#826)

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,32 +1,15 @@
+#!/bin/bash
+
 pkg_list=(
+    'django==3.*'
+    'celery==5.*'
     'ods-tools==2.*'
     'oasislmf==1.26.*'
-    joblib
-    oauthlib
-    parso
-    certifi
-    cryptography
-    wheel
-    ruamel.yaml
-    distlib
-    'sqlalchemy==1.*'
-    'django==3.*'
-    django-celery-results
-    'celery==5.*'
-    virtualenv
-    filelock
-    text-unidecode
-    azure-storage-blob
-    coverage
-    django-request-logging
-    drf-yasg
-    scipy
-    waitress
-    sklearn
-    psycopg2-binary
-    scikit-learn
 )
 
+if [ "$#" -gt 0 ]; then
+    pkg_list=( "$@" )
+fi     
 
 PKG_UPDATE=''
 for pk in "${pkg_list[@]}"; do
