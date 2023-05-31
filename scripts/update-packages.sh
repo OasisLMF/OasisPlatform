1beb219f21394f7fd604bf065bc75cab1976cbbd
sambles <sambles@users.noreply.github.com>
2023-05-31 10:49:20 +0100

    fix-package-updates (#825)
    * fix-package-updates
    
    * Add worker controller
    
    * Updated Package Requirements
    
    ---------
    
    Co-authored-by: awsbuild <awsbuild@oasislmf.org>

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,33 +1,16 @@
+#!/bin/bash
+
 pkg_list=(
     'ods-tools==3.*'
     'oasislmf==1.27.*'
     'pandas==1.*'
-    joblib
-    cryptography
-    oauthlib
-    parso
-    certifi
-    wheel
-    ruamel.yaml
-    distlib
-    'sqlalchemy==1.*'
     'django==3.*'
-    django-celery-results
     'celery==5.*'
-    virtualenv
-    filelock
-    text-unidecode
-    azure-storage-blob
-    coverage
-    django-request-logging
-    drf-yasg
-    scipy
-    waitress
-    sklearn
-    psycopg2-binary
-    scikit-learn
 )
 
+if [ "$#" -gt 0 ]; then
+    pkg_list=( "$@" )
+fi     
 
 PKG_UPDATE=''
 for pk in "${pkg_list[@]}"; do
@@ -38,3 +21,4 @@ set -e
 pip-compile $PKG_UPDATE requirements-worker.in
 pip-compile $PKG_UPDATE requirements-server.in
 pip-compile $PKG_UPDATE requirements.in
+pip-compile $PKG_UPDATE kubernetes/worker-controller/requirements.in
