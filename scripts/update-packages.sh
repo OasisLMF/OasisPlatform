1d6af5cbd04b0d706994e2049c71d83629fe222a
sambles <sambles@users.noreply.github.com>
2023-05-31 11:06:32 +0100

    Add option to select packages to update in workflow (#826)

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,57 +1,20 @@
-#pkg_list=(
-#    'oasislmf==1.15.*'
-#    joblib
-#    oauthlib
-#    parso
-#    certifi
-#    cryptography
-#    wheel
-#    ruamel.yaml
-#    distlib
-#    'sqlalchemy==1.*'
-#    'django==3.*'
-#    django-celery-results
-#    'celery==5.*'
-#    virtualenv
-#    filelock
-#    text-unidecode
-#    azure-storage-blob
-#    coverage
-#    django-request-logging
-#    drf-yasg
-#    scipy
-#    waitress
-#    sklearn
-#    psycopg2-binary
-#    scikit-learn
-#)
-
+#!/bin/bash
 
 pkg_list=(
-    'oasislmf==1.15.*'
     'django==3.*'
     'celery==5.*'
-    certifi
-    cookiecutter
-    'cryptography==3.3.*'
-    #'cryptography==39.*'
-    future
-    ipython
-    py
-    pygments
-    pyjwt
-    sqlparse
-    urllib3
-    waitress
+    'oasislmf==1.15.*'
 )
 
+if [ "$#" -gt 0 ]; then
+    pkg_list=( "$@" )
+fi     
 
 PKG_UPDATE=''
 for pk in "${pkg_list[@]}"; do
     PKG_UPDATE=$PKG_UPDATE" --upgrade-package $pk"   
 done
 
-#rm requirements-worker.txt requirements-server.txt requirements.txt
 set -e
 pip-compile $PKG_UPDATE requirements-worker.in
 pip-compile $PKG_UPDATE requirements-server.in
