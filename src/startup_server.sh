e36d8549c7d3ed7ece189133c149dd9b50cd050a
Sam Gamble <hexadessa@gmail.com>
2023-07-27 16:47:46 +0100

    Add support to migration between plat2 versions

--- src/startup_server.sh
+++ src/startup_server.sh
@@ -9,7 +9,8 @@ wait-for-it ${OASIS_CELERY_DB_HOST}:${OASIS_CELERY_DB_PORT} -t 60
 wait-for-it ${OASIS_CELERY_BROKER_URL} -t 60
 
 if [ "${STARTUP_RUN_MIGRATIONS}" = true ]; then
-    python3 manage.py migrate
+    # Fallback migration between plat2 (older) and plat2 (2.2.1+)
+    python3 manage.py migrate || migration-helper.sh
     # Create default admin if `$OASIS_ADMIN_USER` && `$OASIS_ADMIN_PASS` are set
     python3 set_default_user.py
 fi
