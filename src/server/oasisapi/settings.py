3cb6d012cdc5a0c732d48275bd91188fbf44d4ef
Sam Gamble <hexadessa@gmail.com>
2023-11-28 13:33:03 +0000

    remove django-extentions

--- src/server/oasisapi/settings.py
+++ src/server/oasisapi/settings.py
@@ -80,7 +80,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions',
+#    'django_extensions',
     'django_filters',
     'rest_framework',
     'drf_yasg',
