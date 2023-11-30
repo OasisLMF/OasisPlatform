028296d2149c5a3e9dcd391ede42605e1f8b3fe5
Sam Gamble <hexadessa@gmail.com>
2023-11-30 11:39:21 +0000

    first pass - Split celery tasks

--- src/server/oasisapi/analyses/apps.py
+++ src/server/oasisapi/analyses/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
 
 
-class AnalysesAppConfig(AppConfig):
-    name = 'src.server.oasisapi.analyses'
+class V1_AnalysesAppConfig(AppConfig):
+    name = 'src.server.oasisapi.analyses.v1_api'
+
+class V2_AnalysesAppConfig(AppConfig):
+    name = 'src.server.oasisapi.analyses.v2_api'
 
     def ready(self):
         from django.db.models.signals import post_save
