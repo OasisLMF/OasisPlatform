8dd45eb9d4ef9aff8f4dc6a65aa8dca6cf018451
Sam Gamble <hexadessa@gmail.com>
2023-12-02 21:11:55 +0000

    take pri value from conf_v2 file

--- src/server/oasisapi/celery_app_v2.py
+++ src/server/oasisapi/celery_app_v2.py
@@ -4,6 +4,8 @@ import os
 from celery import Celery
 from django.conf import settings
 from kombu import Queue, Exchange
+from ...conf.celeryconf_v2 import CELERY_QUEUE_MAX_PRIORITY
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.server.oasisapi.settings.v2')
 v2 = Celery('v2', include=['src.server.oasisapi.analyses.v2_api'])
@@ -19,6 +21,6 @@ v2.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 #  Once a V2 task hits either the "v2-worker-monitor" or "v2-task-controller" the 'src.server.oasisapi.settings.v2' settings will be active, 
 #  which will load with the correct default "CELERY_MAX_QUEUE_PRIORITY=10" 
 v2.conf.task_queues = [
-    Queue('celery-v2', Exchange('celery-v2'), routing_key='celery-v2', queue_arguments={'x-max-priority': 10}),
-    Queue('task-controller', Exchange('task-controller'), routing_key='task-controller', queue_arguments={'x-max-priority': 10}),
+    Queue('celery-v2', Exchange('celery-v2'), routing_key='celery-v2', queue_arguments={'x-max-priority': CELERY_QUEUE_MAX_PRIORITY}),
+    Queue('task-controller', Exchange('task-controller'), routing_key='task-controller', queue_arguments={'x-max-priority': CELERY_QUEUE_MAX_PRIORITY}),
 ]
