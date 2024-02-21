e6309ca19c611c1bd9635f1c05a8b1d0f34a8d87
Sam Gamble <hexadessa@gmail.com>
2024-02-21 14:18:31 +0000

    revert - removed WS task update (move to new PR)

--- src/server/oasisapi/analyses/apps.py
+++ src/server/oasisapi/analyses/apps.py
@@ -8,13 +8,9 @@ class V1_AnalysesAppConfig(AppConfig):
 class V2_AnalysesAppConfig(AppConfig):
     name = 'src.server.oasisapi.analyses.v2_api'
 
-    # def ready(self):
-    #     from django.db.models.signals import post_save
-    #     from .v2_api.signal_receivers import task_updated
-    #     from .models import AnalysisTaskStatus
-    #
-    #     """ This sends an update to the web-socket on each update of a subtask and can generate a lot of
-    #         messages under heavy load --- test removing this feature and scaling based on the periodic
-    #         queue updates
-    #     """
-    #      post_save.connect(task_updated, sender=AnalysisTaskStatus)
+    def ready(self):
+        from django.db.models.signals import post_save
+        from .v2_api.signal_receivers import task_updated
+        from .models import AnalysisTaskStatus
+
+        post_save.connect(task_updated, sender=AnalysisTaskStatus)
