6fdfb61f2c4521da10f57a48cca21faba82550ad
Sam Gamble <hexadessa@gmail.com>
2024-05-09 10:34:56 +0100

    Move debug compose file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -62,5 +62,6 @@ fi
 set -e
 docker build -f Dockerfile.api_server $BUILD_ARGS_SERVER -t coreoasis/api_server:dev .
 docker build -f Dockerfile.model_worker $BUILD_ARGS_WORKER -t coreoasis/model_worker:dev .
+
 # docker-compose -f compose/s3.docker-compose.yml up -d
-docker-compose -f up -d
+docker-compose -f compose/debug.docker-compose.yml up -d
