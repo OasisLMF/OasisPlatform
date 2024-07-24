d9d8e4b0b9dc3ce9d4c34258204aaeefa32ce47f
Sam Gamble <hexadessa@gmail.com>
2024-07-24 10:00:55 +0100

    Add back raise_validate_errors method ~ needed in V1

--- src/server/oasisapi/analyses/models.py
+++ src/server/oasisapi/analyses/models.py
@@ -514,6 +514,11 @@ class Analysis(TimeStampedModel):
         self.task_finished = None
         self.save()
 
+
+    def raise_validate_errors(self, errors):
+        raise ValidationError(detail=errors)
+
+
     def generate_and_run(self, initiator):
         valid_choices = [
             self.status_choices.NEW,
