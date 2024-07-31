d4e61770f99cb8630d0afa5ebbdd984cd8dae5cc
Sam Gamble <hexadessa@gmail.com>
2024-07-31 09:22:09 +0100

    Revert "Allow exection cancel to be re-issued if sub-tasks are marked as running or queued"
    This reverts commit bbb2f322785890e0b55154c5cdc1e6b370d36987.

--- src/server/oasisapi/analyses/models.py
+++ src/server/oasisapi/analyses/models.py
@@ -467,7 +467,7 @@ class Analysis(TimeStampedModel):
                 if not self.model.resource_file:
                     errors['model_settings_file'] = ['Must not be null for Dynamic chunking']
                 elif self.settings_file:
-                    # cross check model and analysisAnalysisTaskStatus settings if not given a set of events
+                    # cross check model and analysis settings if not given a set of events
                     model_settings = self.model.resource_file.read_json()
                     analysis_settings = self.settings_file.read_json()
                     if not analysis_settings.get('event_ids'):
@@ -516,18 +516,6 @@ class Analysis(TimeStampedModel):
 
     def raise_validate_errors(self, errors):
         raise ValidationError(detail=errors)
-
-    def has_active_subtasks(self):
-        """ Check if an analysis has sub-tasks in a queued/running state.
-            Return True if yes.
-        """
-        subtask_qs = self.sub_task_statuses.filter(
-            status__in=[
-                AnalysisTaskStatus.status_choices.PENDING,
-                AnalysisTaskStatus.status_choices.QUEUED,
-                AnalysisTaskStatus.status_choices.STARTED]
-        )
-        return (len(subtask_qs) > 0)
 
     def generate_and_run(self, initiator):
         valid_choices = [
@@ -676,13 +664,11 @@ class Analysis(TimeStampedModel):
     def cancel_any(self):
         INPUTS_GENERATION_STATES = [
             self.status_choices.INPUTS_GENERATION_QUEUED,
-            self.status_choices.INPUTS_GENERATION_STARTED,
-            self.status_choices.INPUTS_GENERATION_CANCELLED
+            self.status_choices.INPUTS_GENERATION_STARTED
         ]
         RUN_ANALYSIS_STATES = [
             self.status_choices.RUN_QUEUED,
-            self.status_choices.RUN_STARTED,
-            self.status_choices.RUN_CANCELLED
+            self.status_choices.RUN_STARTED
         ]
         valid_choices = INPUTS_GENERATION_STATES + RUN_ANALYSIS_STATES
         if self.status not in valid_choices:
@@ -697,9 +683,7 @@ class Analysis(TimeStampedModel):
     def cancel_analysis(self):
         valid_choices = [
             self.status_choices.RUN_QUEUED,
-            self.status_choices.RUN_STARTED,
-            self.status_choices.RUN_CANCELLED
-
+            self.status_choices.RUN_STARTED
         ]
         if self.status not in valid_choices:
             raise ValidationError({'status': ['Analysis execution is not running or queued']})
@@ -716,10 +700,8 @@ class Analysis(TimeStampedModel):
     def cancel_generate_inputs(self):
         valid_choices = [
             self.status_choices.INPUTS_GENERATION_QUEUED,
-            self.status_choices.INPUTS_GENERATION_STARTED,
-            self.status_choices.INPUTS_GENERATION_CANCELLED
+            self.status_choices.INPUTS_GENERATION_STARTED
         ]
-
         if self.status not in valid_choices:
             raise ValidationError({'status': ['Analysis input generation is not running or queued']})
 
