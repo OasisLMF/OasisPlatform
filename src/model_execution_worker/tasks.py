1de5ed26060d7f5c7f5f014ff3d08dff15d1b5bc
Sam Gamble <hexadessa@gmail.com>
2024-08-16 10:44:21 +0100

    pep

--- src/model_execution_worker/tasks.py
+++ src/model_execution_worker/tasks.py
@@ -507,7 +507,6 @@ def generate_input(self,
         lookup_validation_fp = next(iter(glob.glob(os.path.join(oasis_files_dir, 'exposure_summary_report.json'))), None)
         summary_levels_fp = next(iter(glob.glob(os.path.join(oasis_files_dir, 'exposure_summary_levels.json'))), None)
 
-
         # Store logs
         traceback = filestore.put(kwargs['log_filename'])
         filestore.get(traceback, os.path.join(oasis_files_dir, 'log', 'v1-generate-oasis-files.txt'))
