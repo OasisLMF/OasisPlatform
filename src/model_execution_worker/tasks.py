83d2e3f102d2a2cd4f0683f584512ff04fd76bdf
Sam Gamble <hexadessa@gmail.com>
2024-11-13 09:54:44 +0000

    Remove the conf variable MODEL_SETTINGS_FILE from run params

--- src/model_execution_worker/tasks.py
+++ src/model_execution_worker/tasks.py
@@ -299,16 +299,10 @@ def start_analysis(analysis_settings, input_location, complex_data_files=None, *
         # oasislmf.json
         config_path = get_oasislmf_config_path(settings)
         config = config_strip_default_exposure(get_json(config_path))
-
-        # model settings
-        model_settings_fp = settings.get('worker', 'MODEL_SETTINGS_FILE', fallback='')
-        model_settings_file = model_settings_fp if model_settings_fp and os.path.isfile(model_settings_fp) else None
-
         task_params = {
             'oasis_files_dir': oasis_files_dir,
             'model_run_dir': run_dir,
             'analysis_settings_json': analysis_settings_file,
-            'model_settings_json': model_settings_file,
             'ktools_fifo_relative': True,
             'verbose': debug_worker,
             # 'df_engine': json.dumps({
