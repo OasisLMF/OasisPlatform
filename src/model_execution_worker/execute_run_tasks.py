2080f976cde40c34cdb52cab871e700ad72e9828
Ha-Ree <harrys_spam@rocketship.com>
2025-03-21 10:49:15 +0000

    signature bug

--- src/model_execution_worker/execute_run_tasks.py
+++ src/model_execution_worker/execute_run_tasks.py
@@ -15,38 +15,30 @@ app.config_from_object(celery_conf)
 def run_exposure_task(loc_filepath, acc_filepath, ri_filepath, rl_filepath, given_params):
     from oasislmf.manager import OasisManager
     from src.model_execution_worker.utils import TemporaryDir
-    print('Task received')
 
-    safe = os.path.join(os.getcwd(), "outfile.csv")
-    with TemporaryDir() as temp_dir:
-        loc_temp = os.path.join(temp_dir, "location.csv")
-        acc_temp = os.path.join(temp_dir, "account.csv")
-        ri_temp = os.path.join(temp_dir, "ri_info.csv") if ri_filepath else None
-        rl_temp = os.path.join(temp_dir, "ri_scope.csv") if rl_filepath else None
-        shutil.copy2(loc_filepath, loc_temp)
-        shutil.copy2(acc_filepath, acc_temp)
-        if ri_filepath:
-            shutil.copy2(ri_filepath, ri_temp)
-        if rl_filepath:
-            shutil.copy2(rl_filepath, rl_temp)
-
-        os.chdir(temp_dir)
-        print(temp_dir)
-        try:
+    try:
+        with TemporaryDir() as temp_dir:
+            loc_temp = os.path.join(temp_dir, "location.csv")
+            acc_temp = os.path.join(temp_dir, "account.csv")
+            ri_temp = os.path.join(temp_dir, "ri_info.csv") if ri_filepath else None
+            rl_temp = os.path.join(temp_dir, "ri_scope.csv") if rl_filepath else None
+            shutil.copy2(loc_filepath, loc_temp)
+            shutil.copy2(acc_filepath, acc_temp)
+            if ri_filepath:
+                shutil.copy2(ri_filepath, ri_temp)
+            if rl_filepath:
+                shutil.copy2(rl_filepath, rl_temp)
+            os.chdir(temp_dir)
+            print(temp_dir)
             params = OasisManager()._params_run_exposure()
             update_params(params, given_params)
             OasisManager().run_exposure(**params)
-            print("Done.", temp_dir)
-            shutil.move("outfile.csv", safe)
-        except Exception as e:
-            print(e)
-            print(e.stderr)
-            print(e.stdout)
-            raise
+            return get_filestore(settings).put("outfile.csv")
 
-    filestore = get_filestore(settings)
-    path = filestore.put(safe)
-    return path
+    except Exception as e:
+        with open("error.txt", "w") as error_file:
+            error_file.write(e)
+        return get_filestore(settings).put("error.txt")
 
 
 def update_params(params, given_params):
--- src/server/oasisapi/portfolios/models.py
+++ src/server/oasisapi/portfolios/models.py
@@ -11,7 +11,7 @@ from rest_framework.reverse import reverse
 from rest_framework.exceptions import ValidationError
 
 from ..files.models import RelatedFile, related_file_to_df
-from src.model_execution_worker.execute_run_tasks import run_exposure_task
+from src.server.oasisapi.celery_app_v2 import v2 as celery_app_v2
 from .v2_api.tasks import record_output
 
 import re
@@ -69,9 +69,8 @@ class Portfolio(TimeStampedModel):
                                               default=None, related_name='reinsurance_info_file_portfolios')
     reinsurance_scope_file = models.ForeignKey(RelatedFile, on_delete=models.CASCADE, blank=True, null=True,
                                                default=None, related_name='reinsurance_scope_file_portfolios')
-    exposure_run_file = models.ForeignKey(
-        RelatedFile, on_delete=models.CASCADE, blank=True, null=True, default=None, related_name='exposure_run_file_portfolios'
-    )
+    exposure_run_file = models.ForeignKey(RelatedFile, on_delete=models.CASCADE, blank=True, null=True,
+                                          default=None, related_name='exposure_run_file_portfolios')
 
     class Meta:
         ordering = ['id']
@@ -185,6 +184,24 @@ class Portfolio(TimeStampedModel):
         else:
             self.set_portolio_valid()
 
+    # def exposure_run_signature(self, params):
+    #     try:
+    #         loc_file_path = self.location_file.file.path
+    #         acc_file_path = self.accounts_file.file.path
+    #     except Exception as e:
+    #         raise ValidationError(f"Exposure run requires a location and an accounts file. {str(e)}")
+    #     ri_file_path = getattr(getattr(self.reinsurance_info_file, 'file', None), 'path', None)
+    #     rl_file_path = getattr(getattr(self.reinsurance_scope_file, 'file', None), 'path', None)
+    #     return celery_app_v2.signature(
+    #         'run_exposure_task',
+    #         args=(loc_file_path, acc_file_path, ri_file_path, rl_file_path, params)
+    #     )
+
+    # def exposure_run(self, params, user_pk):
+    #     task = self.exposure_run_signature(params)
+    #     task.link(record_output.s(self.pk, user_pk))
+    #     task.apply_async(queue='oasis-internal-worker')
+
     def exposure_run(self, params, user_pk):
         try:
             loc_file_path = self.location_file.file.path
@@ -193,11 +210,10 @@ class Portfolio(TimeStampedModel):
             raise ValidationError(f"Exposure run requires a location and an accounts file. {str(e)}")
         ri_file_path = getattr(getattr(self.reinsurance_info_file, 'file', None), 'path', None)
         rl_file_path = getattr(getattr(self.reinsurance_scope_file, 'file', None), 'path', None)
-
+        from src.model_execution_worker.execute_run_tasks import run_exposure_task
         task = run_exposure_task.s(loc_file_path, acc_file_path, ri_file_path, rl_file_path, params)
         task.link(record_output.s(self.pk, user_pk))
         task.apply_async(queue='oasis-internal-worker')
-
 
 class PortfolioStatus(TimeStampedModel):
 
