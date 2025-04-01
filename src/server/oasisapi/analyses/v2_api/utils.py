8d0969865136a064d82112b10cc9d40bbee639f4
Ha-Ree <harrys_spam@rocketship.com>
2025-04-01 14:52:59 +0100

    Adding utils

---
+++ src/server/oasisapi/analyses/v2_api/utils.py
@@ -0,0 +1,4 @@
+def verify_model_scaling(model):
+    if model.run_mode.lower() == "v1":
+        if model.scaling_options.scaling_strategy not in ["QUEUE_LOAD", "FIXED_WORKERS"]:
+            raise ValueError("Model has invalid scaling setting")
