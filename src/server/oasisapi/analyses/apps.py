7122343b5ef2a51fdd06c8b1af2b16bf10bcb7bb
Ha-Ree <harrys_spam@rocketship.com>
2025-04-03 10:46:53 +0100

    some ready stuff

--- src/server/oasisapi/analyses/apps.py
+++ src/server/oasisapi/analyses/apps.py
@@ -4,6 +4,14 @@ from django.apps import AppConfig
 class V1_AnalysesAppConfig(AppConfig):
     name = 'src.server.oasisapi.analyses.v1_api'
 
+    def ready(self):
+        from src.server.oasisapi.signals import default_ready
+        default_ready()
+
 
 class V2_AnalysesAppConfig(AppConfig):
     name = 'src.server.oasisapi.analyses.v2_api'
+
+    def ready(self):
+        from src.server.oasisapi.signals import default_ready
+        default_ready()
