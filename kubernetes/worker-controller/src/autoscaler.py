584a4d202a47b7aa7f478c76e0cfd74b1a5f66a1
Ha-Ree <harrys_spam@rocketship.com>
2025-04-03 11:24:16 +0100

    input generation run

--- kubernetes/worker-controller/src/autoscaler.py
+++ kubernetes/worker-controller/src/autoscaler.py
@@ -200,15 +200,16 @@ class AutoScaler:
 
             for (i, analysis_entry) in enumerate(analyses_list):
                 analysis = analysis_entry['analysis']
-                if is_model_v1(analysis['model'], entry['queue']['models']):
-                    if analysis.get('status') in ['RUN_QUEUED', 'RUN_STARTED']:
+                tasks = analysis.get('sub_task_count', 0)
+                if tasks == 0:
+                    if analysis.get('status') in ['RUN_QUEUED', 'RUN_STARTED', 'INPUTS_GENERATION_QUEUED', 'INPUTS_GENERATION_STARTED']:
                         sa_id = analysis['id']
                         if sa_id not in running_analyses:
                             priority = int(analysis.get('priority', 1))
                             running_analyses[sa_id] = RunningAnalysis(id=analysis['id'], tasks=1,
                                                                       queue_names=[f"{entry['queue']['name']}-v1"], priority=priority)
                             continue
-                tasks = analysis.get('sub_task_count', 0)
+                
                 queue_names = set()
                 task_counts = analysis.get('status_count', {})
                 tasks_in_queue = task_counts.get('TOTAL_IN_QUEUE', 0)
