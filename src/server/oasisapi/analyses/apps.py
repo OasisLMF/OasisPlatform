025f722917cd307124a18dc9691879dc770c9c23
Ha-Ree <harrys_spam@rocketship.com>
2025-04-04 09:38:54 +0100

    Ping on completion

--- src/server/oasisapi/analyses/apps.py
+++ src/server/oasisapi/analyses/apps.py
@@ -1,17 +1,18 @@
 from django.apps import AppConfig
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
 
 
 class V1_AnalysesAppConfig(AppConfig):
     name = 'src.server.oasisapi.analyses.v1_api'
 
     def ready(self):
-        from src.server.oasisapi.signals import default_ready
-        default_ready()
+        from django.db.models.signals import post_save
+        from .v2_api.signal_receivers import analysis_updated
+        from .models import Analysis
+
+        post_save.connect(analysis_updated, sender=Analysis)
 
 
 class V2_AnalysesAppConfig(AppConfig):
     name = 'src.server.oasisapi.analyses.v2_api'
-
-    def ready(self):
-        from src.server.oasisapi.signals import default_ready
-        default_ready()
