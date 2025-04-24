9b43dbed6d59bd4125b03fca15ec72e70dffe92e
sambles <sambles@users.noreply.github.com>
2025-04-24 09:32:14 +0100

    Fix User-data file loading  (#1198)
    * func has refs to outdated filestore class
    
    * clean-up

--- src/model_execution_worker/utils.py
+++ src/model_execution_worker/utils.py
@@ -272,20 +272,8 @@ def prepare_complex_model_file_inputs(complex_model_files, run_directory, filest
         stored_fn = cmf[STORED_FILENAME]
         orig_fn = cmf[ORIGINAL_FILENAME]
 
-        if filestore._is_locally_stored(stored_fn):
-            # If refrence is local filepath check that it exisits and copy/symlink
-            from_path = filestore.filepath(stored_fn)
-            to_path = os.path.join(run_directory, orig_fn)
-            if os.name == 'nt':
-                logging.info(f'complex_model_file: copy {from_path} to {to_path}')
-                shutil.copy(from_path, to_path)
-            else:
-                logging.info(f'complex_model_file: link {from_path} to {to_path}')
-                os.symlink(from_path, to_path)
-        else:
-            # If reference is a remote, then download the file & rename to 'original_filename'
-            fpath = filestore.get(stored_fn, run_directory)
-            shutil.move(fpath, os.path.join(run_directory, orig_fn))
+        fpath = filestore.get(stored_fn, run_directory)
+        shutil.move(fpath, os.path.join(run_directory, orig_fn))
 
 
 def update_params(params, given_params):
