26101915627cb5591824c081518d40bfcdb26434
Ha-Ree <harrys_spam@rocketship.com>
2025-07-11 13:56:30 +0100

    some tests and stuff

--- src/server/oasisapi/analyses/v2_api/tests/test_task_controller.py
+++ src/server/oasisapi/analyses/v2_api/tests/test_task_controller.py
@@ -1,13 +1,13 @@
-# from unittest.mock import patch
-# from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock, patch
 from uuid import uuid4
 
-# from django.test import TestCase
+from django.test import TestCase
 
-# from src.server.oasisapi.analyses.models import AnalysisTaskStatus
-# from src.server.oasisapi.analyses.v2_api.task_controller import TaskParams, Controller
-# from src.server.oasisapi.analyses.v2_api.tests.fakes import fake_analysis
-# from src.server.oasisapi.auth.tests.fakes import fake_user
+from src.server.oasisapi.analyses.v2_api.task_controller import TaskParams, Controller
+from src.server.oasisapi.analyses.v2_api.tests.fakes import fake_analysis
+from src.server.oasisapi.auth.tests.fakes import fake_user
+from celery import signature
+from src.server.oasisapi.analyses.v2_api.tasks import record_sub_task_failure, record_sub_task_success
 
 
 class FakeChord:
@@ -42,297 +42,142 @@ class FakeChord:
         res.parent.children = [self._make_fake_task(_id) for _id in self.task_ids]
         return res
 
-# class TaskController(TestCase):
-#     def test_get_subtask_signature_returns_the_correct_task_name_with_params_and_linked_tasks(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         sig = Controller.get_subtask_signature(
-#             analysis,
-#             initiator,
-#             'the_task',
-#             TaskParams('foo', bar='boo'),
-#             'the_queue'
-#         )
-#
-#         success_callback = sig.options['link']
-#         error_callback = sig.options['link_error']
-#
-#         self.assertEqual(sig.task, 'the_task')
-#         self.assertEqual(sig.args, ('foo', ))
-#         self.assertEqual(sig.kwargs, {'bar': 'boo'})
-#         self.assertEqual(sig.options['queue'], 'the_queue')
-#
-#         self.assertEqual(len(success_callback), 1)
-#         self.assertEqual(success_callback[0].task, 'record_sub_task_success')
-#         self.assertEqual(success_callback[0].args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(success_callback[0].kwargs, {})
-#         self.assertEqual(success_callback[0].options, {})
-#
-#         self.assertEqual(len(error_callback), 1)
-#         self.assertEqual(error_callback[0].task, 'record_sub_task_failure')
-#         self.assertEqual(error_callback[0].args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(error_callback[0].kwargs, {})
-#         self.assertEqual(error_callback[0].options, {})
-#
-#     def test_get_generate_inputs_results_callback_has_correct_signature(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         sig = Controller.get_generate_inputs_results_callback(analysis, initiator)
-#
-#         self.assertEqual(sig.task, 'generate_input_success')
-#         self.assertEqual(sig.args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(sig.kwargs, {})
-#         self.assertEqual(sig.options, {})
-#
-#     def test_get_generate_inputs_failure_callback_has_correct_signature(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         sig = Controller.get_generate_inputs_error_callback(analysis, initiator)
-#
-#         self.assertEqual(sig.task, 'record_generate_inputs_failure')
-#         self.assertEqual(sig.args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(sig.kwargs, {})
-#         self.assertEqual(sig.options, {})
-#
-#     def test_get_generate_losses_results_callback_has_correct_signature(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         sig = Controller.get_generate_losses_results_callback(analysis, initiator)
-#
-#         self.assertEqual(sig.task, 'record_run_analysis_result')
-#         self.assertEqual(sig.args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(sig.kwargs, {})
-#         self.assertEqual(sig.options, {})
-#
-#     def test_get_generate_losses_failure_callback_has_correct_signature(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         sig = Controller.get_generate_losses_error_callback(analysis, initiator)
-#
-#         self.assertEqual(sig.task, 'record_run_analysis_failure')
-#         self.assertEqual(sig.args, (analysis.pk, initiator.pk, ))
-#         self.assertEqual(sig.kwargs, {})
-#         self.assertEqual(sig.options, {})
-#
-#     def test_generate_inputs_with_single_task_param___error_and_success_callbacks_are_chained(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         task_id = uuid4().hex
-#         delay_res = Mock()
-#         delay_res.id = task_id
-#
-#         subtask = Mock()
-#         subtask.delay.return_value = delay_res
-#
-#         class MockController(Controller):
-#             @classmethod
-#             def get_generate_inputs_tasks_params(cls, analysis, initiator):
-#                 return TaskParams('foo', bar='boo')
-#
-#             @classmethod
-#             def get_subtask_signature(cls, task_name, analysis, initiator, run_data_uuid, slug, queue, params):
-#                 return subtask
-#
-#             @classmethod
-#             def get_generate_inputs_results_callback(cls, analysis, initiator):
-#                 return 'generate_inputs_results_callback'
-#
-#             @classmethod
-#             def get_generate_inputs_error_callback(cls, analysis, initiator):
-#                 return 'generate_inputs_error_callback'
-#
-#         MockController.generate_inputs(analysis, initiator)
-#
-#         subtask.s.asset_called_once_with('foo', bar='boo')
-#         subtask.link.assert_called_once_with('generate_inputs_results_callback')
-#         subtask.link_error.assert_called_once_with('generate_inputs_error_callback')
-#         subtask.delay.assert_called_once()
-#         self.assertEqual(analysis.sub_task_statuses.count(), 1)
-#         self.assertTrue(
-#             analysis.sub_task_statuses.filter(
-#                 task_id=task_id, status=AnalysisTaskStatus.status_choices.QUEUED
-#             ).exists()
-#         )
-#
-#     def test_generate_losses_with_single_task_param___error_and_success_callbacks_are_chained(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         task_id = uuid4().hex
-#         delay_res = Mock()
-#         delay_res.id = task_id
-#
-#         subtask = Mock()
-#         subtask.delay.return_value = delay_res
-#
-#         class MockController(Controller):
-#             @classmethod
-#             def get_generate_losses_tasks_params(cls, analysis, initiator):
-#                 return TaskParams('foo', bar='boo')
-#
-#             @classmethod
-#             def get_subtask_signature(cls, task_name, analysis, initiator, run_data_uuid, slug, queue, params):
-#                 return subtask
-#
-#             @classmethod
-#             def get_generate_losses_results_callback(cls, analysis, initiator):
-#                 return 'generate_losses_results_callback'
-#
-#             @classmethod
-#             def get_generate_losses_error_callback(cls, analysis, initiator):
-#                 return 'generate_losses_error_callback'
-#
-#         MockController.generate_losses(analysis, initiator)
-#
-#         subtask.s.asset_called_once_with('foo', bar='boo')
-#         subtask.link.assert_called_once_with('generate_losses_results_callback')
-#         subtask.link_error.assert_called_once_with('generate_losses_error_callback')
-#         subtask.delay.assert_called_once()
-#         self.assertEqual(analysis.sub_task_statuses.count(), 1)
-#         self.assertTrue(
-#             analysis.sub_task_statuses.filter(
-#                 task_id=task_id, status=AnalysisTaskStatus.status_choices.QUEUED
-#             ).exists()
-#         )
-#
-#     def test_generate_inputs_with_multiple_task_params___chord_is_created_correctly(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         subtasks = [
-#             Mock(),
-#             Mock(),
-#             Mock(),
-#             Mock(),
-#             Mock(),
-#             Mock(),
-#             Mock(),
-#         ]
-#
-#         body = Mock()
-#         chain = Mock()
-#         chord = FakeChord()
-#
-#         class MockController(Controller):
-#             @classmethod
-#             #def get_generate_inputs_tasks_params(cls, analysis, initiator):
-#             def get_inputs_generation_tasks(cls, analysis, initiator, run_data_uuid, num_chunks):
-#                 return tuple(zip(
-#                     [
-#                         TaskParams('foo', bar='boo'),
-#                         TaskParams('far', boo='bar'),
-#                     ]
-#                 ))
-#
-#
-#             @classmethod
-#             def get_subtask_signature(cls, task_name, analysis, initiator, run_data_uuid, slug, queue, params):
-#                 return subtasks.pop()
-#
-#             @classmethod
-#             def get_generate_inputs_results_callback(cls, analysis, initiator):
-#                 return body
-#
-#             @classmethod
-#             def get_generate_inputs_error_callback(cls, analysis, initiator):
-#                 return 'generate_inputs_error_callback'
-#
-#             @classmethod
-#             def get_chord_error_callback(cls):
-#                 return 'chord_error_callback'
-#
-#         with patch('src.server.oasisapi.analyses.v2_api.task_controller.chord', chord):
-#         #with patch('src.server.oasisapi.analyses.v2_api.task_controller.chord', chord), \
-#         #     patch('src.server.oasisapi.analyses.v2_api.task_controller.chain', chain):
-#
-#             MockController.generate_inputs(analysis, initiator, loc_lines=10)
-#
-#             self.assertEqual(body.link_error.call_count, 2)
-#             body.link_error.assert_any_call('generate_inputs_error_callback')
-#             body.link_error.assert_any_call('chord_error_callback')
-#
-#             self.assertEqual(len(chord.head), 2)
-#             chord.head[0].s.asset_called_once_with('foo', bar='boo')
-#             chord.head[1].s.asset_called_once_with('far', boo='far')
-#             self.assertEqual(chord.body, body)
-#             self.assertTrue(chord.delay_called)
-#
-#             self.assertEqual(analysis.sub_task_statuses.count(), 2)
-#             self.assertTrue(
-#                 analysis.sub_task_statuses.filter(
-#                     task_id=chord.task_ids[0], status=AnalysisTaskStatus.status_choices.QUEUED
-#                 ).exists()
-#             )
-#             self.assertTrue(
-#                 analysis.sub_task_statuses.filter(
-#                     task_id=chord.task_ids[1], status=AnalysisTaskStatus.status_choices.QUEUED
-#                 ).exists()
-#             )
-#
-#     def test_generate_losses_with_multiple_task_params___chord_is_created_correctly(self):
-#         analysis = fake_analysis()
-#         initiator = fake_user()
-#
-#         subtasks = [
-#             Mock(),
-#             Mock(),
-#         ]
-#
-#         body = Mock()
-#
-#         chord = FakeChord()
-#
-#         class MockController(Controller):
-#             @classmethod
-#             def get_generate_losses_tasks_params(cls, analysis, initiator):
-#                 return [
-#                     TaskParams('foo', bar='boo'),
-#                     TaskParams('far', boo='bar'),
-#                 ]
-#
-#             @classmethod
-#             def get_subtask_signature(cls, task_name, analysis, initiator, run_data_uuid, slug, queue, params):
-#                 return subtasks.pop()
-#
-#             @classmethod
-#             def get_generate_losses_results_callback(cls, analysis, initiator):
-#                 return body
-#
-#             @classmethod
-#             def get_generate_losses_error_callback(cls, analysis, initiator):
-#                 return 'generate_losses_error_callback'
-#
-#             @classmethod
-#             def get_chord_error_callback(cls):
-#                 return 'chord_error_callback'
-#
-#         with patch('src.server.oasisapi.analyses.v2_api.task_controller.chord', chord):
-#             MockController.generate_losses(analysis, initiator)
-#
-#             self.assertEqual(body.link_error.call_count, 2)
-#             body.link_error.assert_any_call('generate_losses_error_callback')
-#             body.link_error.assert_any_call('chord_error_callback')
-#
-#             self.assertEqual(len(chord.head), 2)
-#             chord.head[0].s.asset_called_once_with('foo', bar='boo')
-#             chord.head[1].s.asset_called_once_with('far', boo='far')
-#             self.assertEqual(chord.body, body)
-#             self.assertTrue(chord.delay_called)
-#
-#             self.assertEqual(analysis.sub_task_statuses.count(), 2)
-#             self.assertTrue(
-#                 analysis.sub_task_statuses.filter(
-#                     task_id=chord.task_ids[0], status=AnalysisTaskStatus.status_choices.QUEUED
-#                 ).exists()
-#             )
-#             self.assertTrue(
-#                 analysis.sub_task_statuses.filter(
-#                     task_id=chord.task_ids[1], status=AnalysisTaskStatus.status_choices.QUEUED
-#                 ).exists()
-#             )
+
+class TaskController(TestCase):
+    def test_get_subtask_signature_returns_the_correct_task_name_with_params_and_linked_tasks(self):
+        analysis = fake_analysis(pk=133)
+        initiator = fake_user(pk=82)
+
+        with patch("src.server.oasisapi.analyses.v2_api.task_controller.signature") as mock_sig:
+            mock_sig_return = MagicMock()
+            mock_sig.return_value = mock_sig_return
+            params = TaskParams('foo', bar='bar')
+            signature = Controller.get_subtask_signature('test_task_name', analysis, initiator, 'test_uuid', 'test_slug', 'test_queue', params)
+            self.assertEqual(mock_sig_return, signature)
+            mock_sig.assert_called_once_with(
+                'test_task_name', queue='test_queue', args=params.args, kwargs={
+                    'initiator_id': 82,
+                    'analysis_id': 133,
+                    'slug': 'test_slug',
+                    'run_data_uuid': 'test_uuid',
+                    **params.kwargs
+                })
+            mock_sig_return.link.assert_called_once_with(record_sub_task_success.s(analysis_id=133, initiator_id=82, task_slug='test_slug'))
+            mock_sig_return.link_error.assert_called_once_with(record_sub_task_failure.s(analysis_id=133, initiator_id=82, task_slug='test_slug'))
+
+    def test_start___link_error_chained(self):
+        analysis = MagicMock()
+        analysis.priority = 12
+        analysis.pk = 9
+        analysis.sub_task_statuses = MagicMock()
+        analysis.sub_task_statuses.all = MagicMock()
+        initiator = fake_user()
+        initiator.pk = 28
+        with (patch("src.server.oasisapi.analyses.models.AnalysisTaskStatus.objects.create_statuses"),
+              patch("src.server.oasisapi.analyses.v2_api.task_controller.chain") as mock_chain):
+            create_chain = MagicMock()
+            create_chain.delay = MagicMock()
+            create_chain.delay.return_value = 59
+            mock_chain.return_value = create_chain
+            chain, task = Controller._start(analysis, initiator, {}, {}, 'uuid', 'traceback', 'failure')
+            self.assertEqual(chain, create_chain)
+            create_chain.link_error.assert_called_once_with(signature(
+                'handle_task_failure',
+                kwargs={
+                    'analysis_id': 9,
+                    'initiator_id': 28,
+                    'run_data_uuid': 'uuid',
+                    'traceback_property': 'traceback',
+                    'failure_status': 'failure',
+                }
+            ))
+            create_chain.delay.assert_called_once_with({}, priority=12)
+            self.assertEqual(task, 59)
+
+    def test_generate_inputs___chain_is_created_correctly(self):
+        analysis = MagicMock()
+        initiator = fake_user()
+
+        mock_subtask_1 = MagicMock()
+        mock_subtask_2 = MagicMock()
+        mock_chain = [Mock(), Mock()]
+        analysis.sub_task_statuses = MagicMock()
+        analysis.sub_task_statuses.all.return_value = [mock_subtask_1, mock_subtask_2]
+
+        class MockController(Controller):
+            @classmethod
+            def _get_inputs_generation_chunks(cls, *args):
+                return 2
+
+            @classmethod
+            def get_inputs_generation_tasks(cls, *args):
+                return None, None
+
+            @classmethod
+            def _start(cls, *args):
+                return mock_chain, Mock(id='test_task_id')
+
+            @classmethod
+            def extract_celery_task_ids(cls, *args):
+                return ['id_2', 'id_1']
+
+        result_chain = MockController.generate_inputs(analysis, initiator, loc_lines=10)
+
+        self.assertEqual(result_chain, mock_chain)
+
+        self.assertEqual(mock_subtask_1.task_id, 'id_1')
+        self.assertEqual(mock_subtask_2.task_id, 'id_2')
+        mock_subtask_1.save.assert_called_once()
+        mock_subtask_2.save.assert_called_once()
+
+        self.assertEqual(analysis.lookup_chunks, 2)
+        self.assertEqual(analysis.generate_inputs_task_id, 'test_task_id')
+        analysis.save.assert_called_once_with(update_fields=[
+            "lookup_chunks",
+            "generate_inputs_task_id",
+            "run_task_id"
+        ])
+
+    def test_generate_losses___chain_is_created_correctly(self):
+        analysis = MagicMock()
+        initiator = fake_user()
+        events_total = 1000
+
+        mock_subtask_1 = MagicMock()
+        mock_subtask_2 = MagicMock()
+        mock_chain = [Mock(), Mock()]
+        analysis.sub_task_statuses = MagicMock()
+        analysis.sub_task_statuses.all.return_value = [mock_subtask_1, mock_subtask_2]
+
+        class MockController(Controller):
+            @classmethod
+            def _get_loss_generation_chunks(cls, *args):
+                return 3
+
+            @classmethod
+            def get_loss_generation_tasks(cls, *args):
+                return None, None
+
+            @classmethod
+            def _start(cls, *args):
+                return mock_chain, Mock(id='test_task_id')
+
+            @classmethod
+            def extract_celery_task_ids(cls, *args):
+                return ['id_2', 'id_1']
+
+        result_chain = MockController.generate_losses(analysis, initiator, events_total)
+
+        self.assertEqual(result_chain, mock_chain)
+
+        self.assertEqual(mock_subtask_1.task_id, 'id_1')
+        self.assertEqual(mock_subtask_2.task_id, 'id_2')
+        mock_subtask_1.save.assert_called_once()
+        mock_subtask_2.save.assert_called_once()
+
+        self.assertEqual(analysis.analysis_chunks, 3)
+        self.assertEqual(analysis.run_task_id, 'test_task_id')
+        analysis.save.assert_called_once_with(update_fields=[
+            "analysis_chunks",
+            "run_task_id"
+        ])
