657936e859fcda8966031de76435555b26910ada
Sam Gamble <hexadessa@gmail.com>
2025-09-01 11:51:21 +0100

    mark debugging work in progress

--- src/server/oasisapi/asgi.py
+++ src/server/oasisapi/asgi.py
@@ -5,28 +5,9 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.server.oasisapi.settings")
 django.setup()
 
 # Import your websocket routing
-from src.server.oasisapi.routing import websocket_urlpatterns  # Adjust path as needed
-
-# Create the application
-if os.getenv('OASIS_DISABLE_HTTP', default=True):
-    # Only websocket routing
-    application = ProtocolTypeRouter({
-        'websocket': AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        ),
-    })
-else:
-    # Both HTTP and websocket routing
-    application = ProtocolTypeRouter({
-        'http': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        ),
-    })
+from src.server.oasisapi.routing import application
--- src/server/oasisapi/routing.py
+++ src/server/oasisapi/routing.py
@@ -1,6 +1,8 @@
+from urllib.parse import parse_qs
+import logging
+
 from asgiref.sync import sync_to_async
-from channels.auth import UserLazyObject
-from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.contrib.auth.models import AnonymousUser
 from django.urls import re_path
@@ -15,48 +17,57 @@ url_patterns = [
     re_path('ws/', URLRouter(websocket_urlpatterns))
 ]
 
+logger = logging.getLogger(__name__)
 
-async def get_user(scope):
 
+async def get_user(token_key):
+    logger.info(f'Attempting to authenticate user with token: {token_key}')
     try:
-        header_value = next((v for k, v in scope['headers'] if k == b'authorization'), None)
+        header_value = f'Bearer {token_key}'.encode()
         if not header_value:
+            logger.warning('No token provided, returning AnonymousUser')
             return AnonymousUser()
 
         if settings.API_AUTH_TYPE == 'keycloak':
-
+            logger.info('Using Keycloak authentication')
             backend = KeycloakOIDCAuthenticationBackend()
             authentication = OIDCAuthentication(backend)
-            async_authentication = sync_to_async(authentication.authenticate, thread_sensitive=True)
-
             request = type('', (), {'META': {'HTTP_AUTHORIZATION': header_value}})()
-            user, access_token = await async_authentication(request)
+            user, access_token = await sync_to_async(authentication.authenticate, thread_sensitive=True)(request)
+            logger.info(f'Successfully authenticated user: {user}')
             return user
         else:
-
+            logger.info('Using JWT authentication')
             backend = JWTAuthentication()
             token = backend.get_raw_token(header_value)
             token = backend.get_validated_token(token)
-
-            async_authentication = sync_to_async(backend.get_user, thread_sensitive=True)
-            user = await async_authentication(token)
+            user = await sync_to_async(backend.get_user, thread_sensitive=True)(token)
+            logger.info(f'Successfully authenticated user: {user}')
             return user
     except Exception as e:
+        logger.error(f'Error authenticating user: {e}')
         return AnonymousUser()
 
 
-class JwtAuthMiddleware(BaseMiddleware):
-    def populate_scope(self, scope):
-        # Add it to the scope if it's not there already
-        if 'user' not in scope:
-            scope['user'] = UserLazyObject()
+class TokenAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
 
-    async def resolve_scope(self, scope):
-        scope['user']._wrapped = await get_user(scope)
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get("query_string", b"").decode("utf-8")
+        query_params = parse_qs(query_string)
+        token_key = query_params.get("token", [None])[0]
+
+        if token_key:
+            scope["user"] = await get_user(token_key)
+        else:
+            scope["user"] = AnonymousUser()
+
+        return await self.inner(scope, receive, send)
 
 
 application = ProtocolTypeRouter({
-    'websocket': JwtAuthMiddleware(
+    'websocket': TokenAuthMiddleware(
         URLRouter(
             url_patterns
         )
