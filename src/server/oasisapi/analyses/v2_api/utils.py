76ad4cb3a66de6ad414642cd44900e643d898c20
Sam Gamble <hexadessa@gmail.com>
2025-09-11 09:38:49 +0100

    Add check for none scaling_options and switch exception to validation error

--- src/server/oasisapi/analyses/v2_api/utils.py
+++ src/server/oasisapi/analyses/v2_api/utils.py
@@ -1,5 +1,7 @@
+from rest_framework.exceptions import ValidationError
+
 def verify_model_scaling(model):
     if model.run_mode:
-        if model.run_mode.lower() == "v1":
+        if model.run_mode.lower() == "v1" and model.scaling_options:
             if model.scaling_options.scaling_strategy not in ["QUEUE_LOAD", "FIXED_WORKERS", None]:
-                raise ValueError("Model has invalid scaling setting")
+                raise ValidationError("Model has invalid scaling setting")
