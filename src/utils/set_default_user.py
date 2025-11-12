af324b8b0ea4f5176e24a3a047c7d4a9f0f7be73
Sam Gamble <hexadessa@gmail.com>
2025-11-12 13:44:12 +0000

    fix

--- src/utils/set_default_user.py
+++ src/utils/set_default_user.py
@@ -9,25 +9,26 @@ from django.contrib.auth.models import User
 try:
     # The default django admin user is created here only when "simple" apiAuthType is used.
     # For OIDC, the users are created by the OIDC provider and backend classes.
-
+     
     if not bool(os.environ.get('OASIS_USE_OIDC', False)):
         env_username = os.environ.get('OASIS_SERVICE_USERNAME_OR_ID', '')
         env_password = os.environ.get('OASIS_SERVICE_PASSWORD_OR_SECRET', '')
 
-        # backwards compatilty
+        # backwards compatibly 
         if not env_username:
             env_username = os.environ.get('OASIS_ADMIN_USER', '')
         if not env_password:
-            env_password = os.environ.get('OASIS_ADMIN_PASS', '')
+            env_password = os.environ.get('OASIS_ADMIN_PASSWORD', '')
 
-        try:
-            print('Creating user: "{}"'.format(env_username))
-            u = User.objects.get(username=env_username)
-            u.set_password(env_password)
-            u.save()
-            print('User creation complete')
-        except Exception as e:
-            User.objects.create_superuser(env_username, 'admin@example.com', env_password)
-            print(str(e))
+        if env_username and env_password:
+            try:
+                print('Creating user: "{}"'.format(env_username))
+                u = User.objects.get(username=env_username)
+                u.set_password(env_password)
+                u.save()
+                print('User creation complete')
+            except Exception as e:
+                User.objects.create_superuser(env_username, 'admin@example.com', env_password)
+                print(str(e))
 except KeyError:
     print('User Enviroment vars not set')
