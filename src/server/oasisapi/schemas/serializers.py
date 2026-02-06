dcd097c4e1d9d9b2f640ee3c95959601c3f6aac3
Sam Gamble <hexadessa@gmail.com>
2026-02-06 09:24:11 +0000

    update 1.15.x settings compatibility

--- src/server/oasisapi/schemas/serializers.py
+++ src/server/oasisapi/schemas/serializers.py
@@ -171,21 +171,16 @@ class AnalysisSettingsSerializer(JsonSettingsSerializer):
         self.schemaClass = AnalysisSettingHandler.make()
 
     def validate(self, data):
-
         # Note: Workaround for to support workers 1.15.x and older. With the analysis settings schema change the workers with
         # These are added into existing files as a 'fix' so older workers can run without patching the worker schema
-        # This *SHOULD* be removed at a later date once older models are not longer used
-        compatibility_field_map = {
-            "module_supplier_id": {
-                "updated_to": "model_supplier_id"
-            },
-            "model_version_id": {
-                "updated_to": "model_name_id"
-            },
-        }
-        for key in compatibility_field_map:
-            if key not in data:
-                if compatibility_field_map[key]['updated_to'] in data:
-                    data[key] = data[compatibility_field_map[key]['updated_to']]
+        compatibility_fields = [
+            ("module_supplier_id", "model_supplier_id"),
+            ("model_version_id", "model_name_id"),
+        ]
+        for old_key, new_key in compatibility_fields:
+        if old_key in data and new_key not in data:
+            data[new_key] = data[old_key]
+        if new_key in data and old_key not in data:
+            data[old_key] = data[new_key]
 
         return super(AnalysisSettingsSerializer, self).validate_json(data)
