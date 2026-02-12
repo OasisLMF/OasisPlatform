94b82213a74e75107a6f38f11f37b107f40bab46
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-02-12 12:01:23 +0000

    fix fake portfolio

--- src/server/oasisapi/analyses/v1_api/tests/fakes.py
+++ src/server/oasisapi/analyses/v1_api/tests/fakes.py
@@ -59,4 +59,7 @@ def fake_analysis(**kwargs):
     if kwargs.get('model') is None:
         kwargs['model'] = fake_analysis_model()
 
+    if kwargs.get('portfolio') is None:
+        kwargs['portfolio'] = fake_portfolio()
+
     return baker.make(Analysis, **kwargs)
