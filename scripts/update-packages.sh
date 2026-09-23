df709f9c2f0cc56fbdbd08f04459bc203dcfbc74
Sam Gamble <hexadessa@gmail.com>
2026-09-23 10:56:16 +0100

    switch to uv

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -13,7 +13,7 @@ else
 fi     
 
 set -e
-pip-compile $PKG_UPDATE requirements-worker.in
-pip-compile $PKG_UPDATE requirements-server.in
-pip-compile $PKG_UPDATE requirements.in
-pip-compile $PKG_UPDATE kubernetes/worker-controller/requirements.in
+uv pip compile $PKG_UPDATE requirements-worker.in --output-file requirements-worker.txt
+uv pip compile $PKG_UPDATE requirements-server.in --output-file requirements-server.txt
+uv pip compile $PKG_UPDATE requirements.in --output-file requirements.txt
+uv pip compile $PKG_UPDATE kubernetes/worker-controller/requirements.in --output-file kubernetes/worker-controller/requirements.txt
